420a589a134693f79348d9401a80785adc8ee2e0
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2002-03-19 23:30:23 +0000

    Put test programs in separate directory

---
+++ trunk/libpqxx/test/test11.cc
@@ -0,0 +1,103 @@
+#include <cstdio>
+#include <iostream>
+
+#include "pg_connection.h"
+#include "pg_transaction.h"
+#include "pg_result.h"
+
+using namespace PGSTD;
+using namespace Pg;
+
+
+// Test program for libpqxx.  Query a table and report its metadata.
+//
+// Usage: test11 [connect-string] [table]
+//
+// Where table is the table to be queried; if none is given, pg_tables is
+// queried by default.
+//
+// The connect-string is a set of connection options in Postgresql's
+// PQconnectdb() format, eg. "dbname=template1" to select from a database
+// called template1, or "host=foo.bar.net user=smith" to connect to a
+// backend running on host foo.bar.net, logging in as user smith.
+int main(int argc, char *argv[])
+{
+  try
+  {
+    const string Table = ((argc >= 3) ? argv[2] : "pg_tables");
+
+    Connection C((argv[1]) ? argv[1] : "");
+    Transaction T(C, "test11");
+
+    Result R( T.Exec(("SELECT * FROM " + Table).c_str()) );
+
+    // Print column names
+    for (Result::Tuple::size_type c = 0; c < R.Columns(); ++c)
+    {
+      string N= R.ColumnName(c);
+      cout << c << ":\t" << N << endl;
+      if (R.ColumnNumber(N) != c)
+	throw logic_error("Expected column '" + N + 
+			  "' to be no. " + ToString(c) + ", "
+			  "but it was no. " + ToString(R.ColumnNumber(N)));
+    }
+
+    // If there are rows in R, compare their metadata to R's.
+    if (!R.empty())
+    {
+      if (R[0].Row() != 0) 
+	throw logic_error("Row 0 said it was row " + R[0].Row());
+
+      if (R.size() < 2)
+	cout << "(Only one row in table.)" << endl;
+      else if (R[1].Row() != 1)
+        throw logic_error("Row 1 said it was row " + R[1].Row());
+
+      for (Result::Tuple::size_type c = 0; c < R[0].size(); ++c)
+      {
+	string N = R.ColumnName(c);
+
+	if (string(R[0].at(c).c_str()) != R[0].at(N).c_str())
+          throw logic_error("Field " + ToString(c) + " contains "
+			    "'" + R[0].at(c).c_str() + "'; "
+			    "field '" + N + "' "
+			    "contains '" + R[0].at(N).c_str() + "'");
+	if (string(R[0][c].c_str()) != R[0][N].c_str())
+          throw logic_error("Field " + ToString(c) + " ('" + N + "'): "
+			    "at() inconsistent with operator[]!");
+
+	if (R[0][c].name() != N)
+	  throw logic_error("Field " + ToString(c) + " "
+			    "called '" + N + "' by Result, "
+			    "but '" + R[0][c].name() + "' by Field object");
+
+	if (size_t(R[0][c].size()) != strlen(R[0][c].c_str()))
+	  throw logic_error("Field '" + N + "' "
+			    "says its length is " + ToString(R[0][c].size()) + 
+			    ", "
+			    "but its value is '" + R[0][c].c_str() + "' "
+			    "(" + ToString(strlen(R[0][c].c_str())) + " "
+			    "chars)");
+      }
+    }
+    else
+    {
+      cout << "(Table is empty.)" << endl;
+    }
+  }
+  catch (const exception &e)
+  {
+    // All exceptions thrown by libpqxx are derived from std::exception
+    cerr << "Exception: " << e.what() << endl;
+    return 2;
+  }
+  catch (...)
+  {
+    // This is really unexpected (see above)
+    cerr << "Unhandled exception" << endl;
+    return 100;
+  }
+
+  return 0;
+}
+
---
+++ trunk/libpqxx/test/test20.cc
@@ -0,0 +1,110 @@
+#include <cassert>
+#include <iostream>
+
+#include "pg_connection.h"
+#include "pg_nontransaction.h"
+#include "pg_result.h"
+
+using namespace PGSTD;
+using namespace Pg;
+
+
+// Test program for libpqxx.  Modify the database inside a NonTransaction, and
+// verify that the change gets made regardless of whether the NonTransaction is 
+// eventually committed or aborted.
+//
+// Usage: test20 [connect-string] [table]
+//
+// Where connect-string is a set of connection options in Postgresql's
+// PQconnectdb() format, eg. "dbname=template1" to select from a database
+// called template1, or "host=foo.bar.net user=smith" to connect to a
+// backend running on host foo.bar.net, logging in as user smith.
+//
+// The table argument is the table being modified.  This defaults to "events".
+// It is assumed to consist of an integer key called year, and a string.
+namespace
+{
+int BoringYear = 1977;
+
+} // namespace
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    Connection C(argv[1] ? argv[1] : "");
+
+    const string Table = ((argc > 2) ? argv[2] : "events");
+
+    {
+      // Begin a transaction acting on our current connection
+      NonTransaction T1(C, "T1");
+
+      // Verify our start condition before beginning: there must not be a 1977
+      // record already.
+      Result R( T1.Exec(("SELECT * FROM " + Table + " "
+	                "WHERE year=" + ToString(BoringYear)).c_str()) );
+      if (R.size() != 0)
+        throw runtime_error("There is already a record for 1977. "
+		            "Can't run test.");
+
+      // OK.  Having laid that worry to rest, add a record for 1977.
+      T1.Exec(("INSERT INTO " + Table + " VALUES"
+               "(" +
+	       ToString(BoringYear) + ","
+	       "'Yawn'"
+	       ")").c_str());
+
+      // Abort T1.  Since T1 is a NonTransaction, which provides only the
+      // transaction class interface without providing any form of transactional
+      // integrity, this is not going to undo our work.
+      T1.Abort();
+    }
+
+    {
+      // Verify that our record was added, despite the Abort()
+      NonTransaction T2(C, "T2");
+      Result R( T2.Exec(("SELECT * FROM " + Table + " "
+		         "WHERE year=" + ToString(BoringYear)).c_str()) );
+      if (R.size() != 1)
+        throw runtime_error("Expected to find 1 record for " + 
+		            ToString(BoringYear) + ", found " +
+			    ToString(R.size()) + ". "
+			    "This could be a bug in libpqxx, "
+			    "or something else modified the table.");
+
+      // Now remove our record again
+      T2.Exec(("DELETE FROM " + Table + " "
+	       "WHERE year=" + ToString(BoringYear)).c_str());
+
+      T2.Commit();
+    }
+
+    {
+      // And again, verify results
+      NonTransaction T3(C, "T3");
+      Result R( T3.Exec(("SELECT * FROM " + Table + " "
+	                 "WHERE year=" + ToString(BoringYear)).c_str()) );
+      if (R.size() != 0)
+        throw runtime_error("Expected record for " + ToString(BoringYear) + " "
+		            "to be gone but found " + ToString(R.size()) + ". "
+			    "This could be a bug in libpqxx, "
+			    "or something else modified the table.");
+    }
+  }
+  catch (const exception &e)
+  {
+    // All exceptions thrown by libpqxx are derived from std::exception
+    cerr << "Exception: " << e.what() << endl;
+    return 2;
+  }
+  catch (...)
+  {
+    // This is really unexpected (see above)
+    cerr << "Unhandled exception" << endl;
+    return 100;
+  }
+
+  return 0;
+}
+
