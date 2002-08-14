b5ea0e0e918dc76e8cd2788407541c73ffae8191
Jeroen Vermeulen <jtv@users.noreply.github.com>
2002-08-14 23:30:18 +0000

    Debianization changes by Ray Dassen

--- libpqxx/autogen.sh
+++ libpqxx/autogen.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-aclocal
+ver=""
+if test -x /usr/bin/automake-1.6 ; then
+	ver="-1.6"
+fi
+aclocal${ver}
 autoheader
 libtoolize --automake --copy
-automake --verbose --add-missing --copy
+automake${ver} --verbose --add-missing --copy
 autoconf
 
 ./configure --enable-maintainer-mode
