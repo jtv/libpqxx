d7e3e5197bd0dd9b0615436b771ad65f539e34b9
Jeroen Vermeulen <jtv@users.noreply.github.com>
2003-01-02 02:30:43 +0000

    Make use of Cursor's absolute positioning.  NOT WORKING YET!

--- libpqxx/src/cachedresult.cxx
+++ libpqxx/src/cachedresult.cxx
@@ -25,21 +25,12 @@ pqxx::CachedResult::CachedResult(pqxx::TransactionItf &Trans,
   m_Granularity(Granularity),
   m_Cache(),
   m_Cursor(Trans, Query, BaseName, Granularity),
-  m_Pos(0), 
-  m_Size(-1),
-  m_Lower(-1),
-  m_Upper(-1)
+  m_Size(size_unknown)
 {
   // We can't accept granularity of 1 here, because some block number 
   // arithmetic might overflow.
   if (m_Granularity <= 1)
     throw out_of_range("Invalid CachedResult granularity");
-
-  if (m_Lower > 0)
-    throw logic_error("Internal libpqxx error: "
-	              "CachedResult::size_type is unsigned");
-
-  m_Upper = BlockFor(numeric_limits<size_type>::max()) + 1;
 }
 
 
@@ -52,112 +43,32 @@ void pqxx::CachedResult::clear()
 void pqxx::CachedResult::MoveTo(blocknum Block) const
 {
   if (Block < 0)
-    throw out_of_range("Invalid result set index");
+    throw out_of_range("Negative result set index");
 
-  if (Block == m_Pos) return;
-  try
-  {
-    if (m_Pos == -1)
-    {
-      // We don't know where we are.  Go back to our origin first.
-      // Hope this does indeed get us back to our starting point...
-      m_Cursor.Move(Cursor::BACKWARD_ALL());
-      m_Pos = 0;
-    }
-    if (Block != m_Pos) m_Cursor.Move((Block - m_Pos) * m_Granularity);
-    m_Pos = Block;
-  }
-  catch (const exception &)
-  {
-    // After an error, we don't know for sure where our cursor is
-    m_Pos = -1;
-    throw;
-  }
+  m_Cursor.MoveTo(FirstRowOf(Block));
 }
 
 
 pqxx::Result pqxx::CachedResult::Fetch() const
 {
   Result R;
-  if (m_Pos == -1) 
-    throw logic_error("Internal libpqxx error: "
-	              "CachedResult fetches data from unknown cursor position");
+  size_type Pos = m_Cursor.Pos();
 
-  if (m_Pos >= m_Upper) 
+  if (m_Cursor >> R) m_Cache.insert(make_pair(BlockFor(Pos), CacheEntry(R)));
+
+  if ((m_Size == size_unknown) && (R.size() < m_Granularity))
   {
-    m_Pos = -1;
-    return R;
+    // This is the last block.  While we're here, record result set size.
+    m_Size = m_Cursor.Pos();
   }
 
-  try
-  {
-    if (!(m_Cursor >> R))
-    {
-      m_Upper = m_Pos;
-      if ((m_Size == -1) && (m_Pos == (m_Lower + 1))) 
-      {
-        m_Size = m_Pos * m_Granularity;
-      }
-
-      m_Pos = -1;
-      return R;
-    }
-    m_Cache.insert(make_pair(m_Pos, CacheEntry(R)));
-    if (m_Pos > m_Lower) m_Lower = m_Pos;
-    if ((m_Size == -1) && (R.size() < m_Granularity))
-    {
-      // This is the last block.  While we're here, record result set size.
-      m_Upper = m_Pos + 1;
-      m_Size = m_Pos * m_Granularity + R.size();
-      m_Pos = -1;
-    }
-    else
-    {
-      ++m_Pos;
-    }
-  }
-  catch (const exception &)
-  {
-    // After an error, we don't know for sure where our cursor is
-    m_Pos = -1;
-    throw;
-  }
   return R;
 }
 
 
 void pqxx::CachedResult::DetermineSize() const
 {
-  if (m_Size != -1) return;
-
-  // Binary search for highest-numbered block we can fetch
-
-  if (m_Cache.empty()) 
-  {
-    GetBlock(0);
-    if (m_Cache.empty())
-    {
-      m_Size = 0;
-      m_Upper = 0;
-      return;
-    }
-  }
-
-  const blocknum TopBlock = BlockFor(numeric_limits<size_type>::max());
-  if (m_Upper > TopBlock)
-  {
-    Result T = GetBlock(TopBlock);
-    if (!T.empty())
-    {
-      if (m_Size <= 0)
-	throw runtime_error("Result set too large");
-      return;
-    }
-  }
-
-  while ((m_Lower+1) < m_Upper) GetBlock((m_Lower + m_Upper)/2);
-
-  CacheMap::reverse_iterator L = m_Cache.rbegin();
-  m_Size = L->first * m_Granularity + L->second.Data().size();
+  m_Cursor.Move(Cursor::ALL());
+  m_Size = -m_Cursor.Move(Cursor::BACKWARD_ALL()) - 1;
 }
 
