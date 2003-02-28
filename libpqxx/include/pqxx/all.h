baac74e965940ac88f2072b0b39ee41b0cac174f
Jeroen Vermeulen <jtv@users.noreply.github.com>
2003-02-28 15:02:03 +0000

    New convenience header

---
+++ libpqxx/include/pqxx/all.h
@@ -0,0 +1,23 @@
+/*-------------------------------------------------------------------------
+ *
+ *   FILE
+ *	pqxx/all.h
+ *
+ *   DESCRIPTION
+ *      Convenience header: include all libpqxx definitions
+ *
+ * Copyright (c) 2003, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ *
+ *-------------------------------------------------------------------------
+ */
+#include <pqxx/cachedresult.h>
+#include <pqxx/connection.h>
+#include <pqxx/cursor.h>
+#include <pqxx/transaction.h>
+#include <pqxx/nontransaction.h>
+#include <pqxx/robusttransaction.h>
+#include <pqxx/transactor.h>
+#include <pqxx/tablereader.h>
+#include <pqxx/tablewriter.h>
+#include <pqxx/trigger.h>
+
