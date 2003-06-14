c09d2bfcc6edcb0cd590b8cae6a2d469e34d585b
Jeroen Vermeulen <jtv@users.noreply.github.com>
2003-06-14 01:47:22 +0000

    *** empty log message ***

---
+++ libpqxx/test/test057.cxx
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <sstream>
+
+#include <pqxx/all.h>
+
+using namespace PGSTD;
+using namespace pqxx;
+
+namespace
+{
+
+/* Read contents of Stream into a single string.  The data will go through
+ * input formatting, so whitespace will be taken as separators between chunks
+ * of data.
+ */
+template<typename T> string UnStream(T &Stream)
+{
+  string Result, X;
+  while (Stream >> X) Result += X;
+  return Result;
+}
+
+class WriteLargeObject : public transactor<>
+{
+public:
+  WriteLargeObject(const string &Contents, largeobject &O) : 
+    transactor<>("WriteLargeObject"),
+    m_Contents(Contents),
+    m_Object(),
+    m_ObjectOutput(O)
+  {
+  }
+
+  void operator()(argument_type &T)
+  {
+    m_Object = largeobject(T);
+    cout << "Created large object #" << m_Object.id() << endl;
+
+    olostream S(T, m_Object.id());
+    S << m_Contents;
+  }
+
+  void OnCommit()
+  {
+    m_ObjectOutput = m_Object;
+  }
+
+private:
+  string m_Contents;
+  largeobject m_Object;
+  largeobject &m_ObjectOutput;
+};
+
+
+class ReadLargeObject : public transactor<>
+{
+public:
+  ReadLargeObject(string &Contents, largeobject O) :
+    transactor<>("ReadLargeObject"),
+    m_Contents(),
+    m_ContentsOutput(Contents),
+    m_Object(O)
+  {
+  }
+
+  void operator()(argument_type &T)
+  {
+    ilostream S(T, m_Object);
+    m_Contents = UnStream(S);
+  }
+
+  void OnCommit()
+  {
+    m_ContentsOutput = m_Contents;
+  }
+
+private:
+  string m_Contents;
+  string &m_ContentsOutput;
+  largeobject m_Object;
+};
+
+
+class DeleteLargeObject : public transactor<>
+{
+public:
+  DeleteLargeObject(largeobject O) : m_Object(O) {}
+
+  void operator()(argument_type &T)
+  {
+    m_Object.remove(T);
+  }
+
+private:
+  largeobject m_Object;
+};
+
+
+}
+
+
+// Simple test program for libpqxx's Large Objects interface.
+//
+// Usage: test057 [connect-string]
+//
+// Where connect-string is a set of connection options in Postgresql's
+// PQconnectdb() format, eg. "dbname=template1" to select from a database
+// called template1, or "host=foo.bar.net user=smith" to connect to a
+// backend running on host foo.bar.net, logging in as user smith.
+int main(int, char *argv[])
+{
+  try
+  {
+    connection C(argv[1]);
+
+    largeobject Obj(oid_none);
+    const string Contents = "Testing, testing, 1-2-3";
+
+    C.Perform(WriteLargeObject(Contents, Obj));
+
+    string Readback;		// Contents as read back from large object
+    C.Perform(ReadLargeObject(Readback, Obj));
+
+    C.Perform(DeleteLargeObject(Obj));
+
+    /* Reconstruct what will happen to our contents string if we put it into a
+     * stream and then read it back.  We can compare this with what comes back
+     * from our large object stream.
+     */
+    stringstream TestStream;
+    TestStream << Contents;
+    const string StreamedContents = UnStream(TestStream);
+
+    cout << StreamedContents << endl << Readback << endl;
+
+    if (Readback != StreamedContents)
+      throw logic_error("Large objects: expected to read "
+	                "'" + StreamedContents + "', "
+			"got '" + Readback + "'");
+  }
+  catch (const sql_error &e)
+  {
+    cerr << "SQL error: " << e.what() << endl
+         << "Query was: '" << e.query() << "'" << endl;
+    return 1;
+  }
+  catch (const exception &e)
+  {
+    cerr << "Exception: " << e.what() << endl;
+    return 2;
+  }
+  catch (...)
+  {
+    cerr << "Unhandled exception" << endl;
+    return 100;
+  }
+
+  return 0;
+}
+
+
---
+++ libpqxx/test/test058.cxx
@@ -0,0 +1,124 @@
+#include <cerrno>
+#include <cstring>
+#include <iostream>
+
+#include <pqxx/all.h>
+#include <pqxx/largeobject.h>
+
+using namespace PGSTD;
+using namespace pqxx;
+
+namespace
+{
+
+const string Contents = "Large object test contents";
+
+class WriteLargeObject : public transactor<>
+{
+public:
+  explicit WriteLargeObject() : 
+    transactor<>("WriteLargeObject")
+  {
+  }
+
+  void operator()(argument_type &T)
+  {
+    largeobjectaccess A(T);
+    cout << "Writing to large object #" << largeobject(A).id() << endl;
+
+    A.write(Contents);
+
+    char Buf[200];
+    const size_t Size = sizeof(Buf) - 1;
+    largeobjectaccess::size_type Bytes = A.read(Buf, Size);
+    if (Bytes)
+      throw logic_error("Could read " + ToString(Bytes) + " bytes "
+	                "from large object after writing");
+
+    // Overwrite terminating zero
+    largeobjectaccess::size_type Here = A.seek(-1, ios::cur);
+    if (Here != Contents.size()-1)
+      throw logic_error("Expected to move back 1 byte to " + 
+	                ToString(Contents.size()-1) + ", "
+			"ended up at " + ToString(Here));
+    A.write("!", 1);
+    
+    // Now check that we really did
+    A.seek(-1, ios::cur);
+    if (Here != Contents.size()-1)
+      throw logic_error("Inconsistent seek: ended up at " + ToString(Here));
+
+    char Check;
+    Here = A.read(&Check, 1);
+    if (Here != 1)
+      throw logic_error("Wanted to read back 1 byte, got " + ToString(Here));
+
+    if (Check != '!')
+      throw logic_error("Read back '" + ToString(Check) + "', "
+	                "expected '!'");
+
+    Here = A.seek(0, ios::beg);
+    if (Here != 0)
+      throw logic_error("Tried to seek back to beginning of large object, "
+	                "ended up at " + ToString(Here));
+
+    Here = A.read(&Check, 1);
+    if (Here != 1)
+      throw logic_error("Tried to read back 1st byte, got " + 
+	                ToString(Here) + " bytes");
+
+    if (Check != Contents[0])
+      throw logic_error("Expected large object to begin with '" +
+	                ToString(Contents[0]) + "', "
+			"found '" + ToString(Check) + "'");
+
+    // Clean up after ourselves
+    A.remove(T);
+  }
+
+private:
+  largeobject m_Object;
+};
+
+}
+
+
+// Mixed-mode, seeking test program for libpqxx's Large Objects interface.
+//
+// Usage: test058 [connect-string]
+//
+// Where connect-string is a set of connection options in Postgresql's
+// PQconnectdb() format, eg. "dbname=template1" to select from a database
+// called template1, or "host=foo.bar.net user=smith" to connect to a
+// backend running on host foo.bar.net, logging in as user smith.
+int main(int, char *argv[])
+{
+  try
+  {
+    connection C(argv[1]);
+
+    largeobject Obj;
+
+    C.Perform(WriteLargeObject());
+  }
+  catch (const sql_error &e)
+  {
+    cerr << "SQL error: " << e.what() << endl
+         << "Query was: '" << e.query() << "'" << endl;
+    return 1;
+  }
+  catch (const exception &e)
+  {
+    cerr << "Exception: " << e.what() << endl;
+    return 2;
+  }
+  catch (...)
+  {
+    cerr << "Unhandled exception" << endl;
+    return 100;
+  }
+
+  return 0;
+}
+
+
