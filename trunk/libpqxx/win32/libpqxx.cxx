7efe56f2d3cf2dfaeb456b1aa44170cf9145c056
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2004-08-01 23:44:52 +0000

    Removed unnecessary crud (essentially everything)

--- trunk/libpqxx/win32/libpqxx.cxx
+++ trunk/libpqxx/win32/libpqxx.cxx
@@ -1,23 +1,11 @@
 // libpqxx.cxx : Defines the entry point for the DLL application.
-//
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <windows.h>
 
-BOOL APIENTRY DllMain( HANDLE hModule, 
-                       DWORD  ul_reason_for_call, 
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HANDLE, DWORD, LPVOID)
 {
-    switch (ul_reason_for_call)
-	{
-		case DLL_PROCESS_ATTACH:
-		case DLL_THREAD_ATTACH:
-		case DLL_THREAD_DETACH:
-		case DLL_PROCESS_DETACH:
-			break;
-    }
     return TRUE;
 }
 
