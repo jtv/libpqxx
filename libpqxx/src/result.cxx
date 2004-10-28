3d3bc7194edac723f532e5762f00cf7b3d627f93
Jeroen Vermeulen <jtv@users.noreply.github.com>
2004-10-28 14:11:46 +0000

    Use new refcounting in PQAlloc; home-grown reverse_iterator types; much more

--- libpqxx/src/result.cxx
+++ libpqxx/src/result.cxx
@@ -27,29 +27,6 @@ using namespace PGSTD;
 using namespace pqxx::internal::pq;
 
 
-pqxx::result &pqxx::result::operator=(const pqxx::result &Other) throw ()
-{
-  if (&Other != this)
-  {
-    LoseRef();
-    MakeRef(Other);
-  }
-
-  return *this;
-}
-
-
-pqxx::result &pqxx::result::operator=(internal::pq::PGresult *Other) throw ()
-{
-  if (Other != m_Result)
-  {
-    LoseRef();
-    MakeRef(Other);
-  }
-  return *this;
-}
-
-
 bool pqxx::result::operator==(const result &rhs) const throw ()
 {
   if (&rhs == this) return true;
@@ -61,56 +38,32 @@ bool pqxx::result::operator==(const result &rhs) const throw ()
 }
 
 
-bool pqxx::result::operator<(const result &rhs) const throw ()
-{
-  if (&rhs == this) return false;
-  const size_type s = size();
-  // TODO: Is this correct?
-  if (s > rhs.size()) return false;
-  for (size_type i=0; i<s; ++i) if (!((*this)[i] < rhs[i])) return false;
-  return true;
-}
-
-
 bool pqxx::result::tuple::operator==(const tuple &rhs) const throw ()
 {
   if (&rhs == this) return true;
   const size_type s(size());
   if (rhs.size() != s) return false;
-  for (size_type i=0; i<s; ++i)
-  {
-    const field l((*this)[i]), r(rhs[i]);
-    if (l.size() != r.size() || l.is_null() != r.is_null()) return false;
-    if (!l.is_null() && (l.as<string>() != r.as<string>())) return false;
-  }
+  // TODO: Depends on how null is handled!
+  for (size_type i=0; i<s; ++i) if ((*this)[i] != rhs[i]) return false;
   return true;
 }
 
 
-bool pqxx::result::tuple::operator<(const tuple &rhs) const throw ()
+bool pqxx::result::field::operator==(const field &rhs) const
 {
-  if (&rhs == this) return false;
+  if (is_null() != rhs.is_null()) return false;
+  // TODO: Verify null handling decision
   const size_type s = size();
-  // TODO: Is this correct?
-  if (s > rhs.size()) return false;
-  // TODO: How to handle nulls!?
-  for (size_type i=0; i<s; ++i)
-    if (!((*this)[i].as<string>() < rhs[i].as<string>())) return false;
+  if (s != rhs.size()) return false;
+  const char *const l(c_str()), *const r(rhs.c_str());
+  for (size_type i = 0; i < s; ++i) if (l[i] != r[i]) return false;
   return true;
 }
 
 
-void pqxx::result::swap(pqxx::result &other) throw ()
+void pqxx::result::swap(result &rhs) throw ()
 {
-  const result *const l=m_l, *const r=m_r;
-  internal::pq::PGresult *p(m_Result);
-
-  m_l = other.m_l;
-  m_r = other.m_r;
-  m_Result = other.m_Result;
-  other.m_l = l;
-  other.m_r = r;
-  other.m_Result = p;
+  super::swap(rhs);
 }
 
 
@@ -140,12 +93,12 @@ void pqxx::result::CheckStatus(const char Query[]) const
 
 string pqxx::result::StatusError() const
 {
-  if (!m_Result)
+  if (!c_ptr())
     throw runtime_error("No result");
 
   string Err;
 
-  switch (PQresultStatus(m_Result))
+  switch (PQresultStatus(c_ptr()))
   {
   case PGRES_EMPTY_QUERY: // The string sent to the backend was empty.
   case PGRES_COMMAND_OK: // Successful completion of a command returning no data
@@ -159,48 +112,21 @@ string pqxx::result::StatusError() const
   case PGRES_BAD_RESPONSE: // The server's response was not understood
   case PGRES_NONFATAL_ERROR:
   case PGRES_FATAL_ERROR:
-    Err = PQresultErrorMessage(m_Result);
+    Err = PQresultErrorMessage(c_ptr());
     break;
 
   default:
     throw logic_error("libpqxx internal error: "
 		      "pqxx::result: Unrecognized response code " +
-		      to_string(int(PQresultStatus(m_Result))));
+		      to_string(int(PQresultStatus(c_ptr()))));
   }
   return Err;
 }
 
 
-void pqxx::result::MakeRef(internal::pq::PGresult *Other) throw ()
-{
-  m_Result = Other;
-}
-
-
-void pqxx::result::MakeRef(const pqxx::result &Other) throw ()
-{
-  m_l = &Other;
-  m_r = Other.m_r;
-
-  m_l->m_r = m_r->m_l = this;
-  m_Result = Other.m_Result;
-}
-
-
-void pqxx::result::LoseRef() throw ()
-{
-  if ((m_l == this) && m_Result) PQclear(m_Result);
-  m_Result = 0;
-  m_l->m_r = m_r;
-  m_r->m_l = m_l;
-  m_l = m_r = this;
-}
-
-
-
 pqxx::result::size_type pqxx::result::affected_rows() const
 {
-  const char *const RowsStr = PQcmdTuples(m_Result);
+  const char *const RowsStr = PQcmdTuples(c_ptr());
   return RowsStr[0] ? atoi(RowsStr) : 0;
 }
 
@@ -208,21 +134,21 @@ pqxx::result::size_type pqxx::result::affected_rows() const
 const char *pqxx::result::GetValue(pqxx::result::size_type Row, 
 		                 pqxx::result::tuple::size_type Col) const
 {
-  return PQgetvalue(m_Result, Row, Col);
+  return PQgetvalue(c_ptr(), Row, Col);
 }
 
 
 bool pqxx::result::GetIsNull(pqxx::result::size_type Row,
 		           pqxx::result::tuple::size_type Col) const
 {
-  return PQgetisnull(m_Result, Row, Col) != 0;
+  return PQgetisnull(c_ptr(), Row, Col) != 0;
 }
 
 pqxx::result::field::size_type 
 pqxx::result::GetLength(pqxx::result::size_type Row,
                         pqxx::result::tuple::size_type Col) const
 {
-  return PQgetlength(m_Result, Row, Col);
+  return PQgetlength(c_ptr(), Row, Col);
 }
 
 
@@ -230,9 +156,9 @@ int pqxx::result::errorposition() const throw ()
 {
   int pos = -1;
 #if defined(PQXX_HAVE_PQRESULTERRORFIELD)
-  if (m_Result)
+  if (c_ptr())
   {
-    const char *p = PQresultErrorField(m_Result, PG_DIAG_STATEMENT_POSITION);
+    const char *p = PQresultErrorField(c_ptr(), PG_DIAG_STATEMENT_POSITION);
     if (p) from_string(p, pos);
   }
 #endif // PQXX_HAVE_PQRESULTERRORFIELD
@@ -271,7 +197,7 @@ pqxx::result::tuple::at(pqxx::result::tuple::size_type i) const throw (out_of_ra
 const char *
 pqxx::result::column_name(pqxx::result::tuple::size_type Number) const
 {
-  const char *const N = PQfname(m_Result, Number);
+  const char *const N = PQfname(c_ptr(), Number);
   if (!N) 
     throw out_of_range("Invalid column number: " + to_string(Number));
 
@@ -282,7 +208,7 @@ pqxx::result::column_name(pqxx::result::tuple::size_type Number) const
 pqxx::result::tuple::size_type
 pqxx::result::column_number(const char ColName[]) const
 {
-  const int N = PQfnumber(m_Result, ColName);
+  const int N = PQfnumber(c_ptr(), ColName);
   if (N == -1)
     throw invalid_argument("Unknown column name: '" + string(ColName) + "'");
 
@@ -292,7 +218,8 @@ pqxx::result::column_number(const char ColName[]) const
 
 // const_iterator
 
-pqxx::result::const_iterator pqxx::result::const_iterator::operator++(int)
+pqxx::result::const_iterator
+pqxx::result::const_iterator::operator++(int)
 {
   const_iterator old(*this);
   m_Index++;
@@ -300,7 +227,8 @@ pqxx::result::const_iterator pqxx::result::const_iterator::operator++(int)
 }
 
 
-pqxx::result::const_iterator pqxx::result::const_iterator::operator--(int)
+pqxx::result::const_iterator
+pqxx::result::const_iterator::operator--(int)
 {
   const_iterator old(*this);
   m_Index--;
