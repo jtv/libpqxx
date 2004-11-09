046215863fdb57b86b9ab552db5e371130d05512
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2004-11-09 14:20:35 +0000

    Added dbtransaction.cxx

---
+++ trunk/libpqxx/src/dbtransaction.cxx
@@ -0,0 +1,56 @@
+/*-------------------------------------------------------------------------
+ *
+ *   FILE
+ *	dbtransaction.cxx
+ *
+ *   DESCRIPTION
+ *      implementation of the pqxx::dbtransaction class.
+ *   pqxx::dbtransaction represents a real backend transaction
+ *
+ * Copyright (c) 2004, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ *
+ * See COPYING for copyright license.  If you did not receive a file called
+ * COPYING with this source code, please notify the distributor of this mistake,
+ * or contact the author.
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "pqxx/dbtransaction"
+
+using namespace std;
+
+
+pqxx::dbtransaction::dbtransaction(connection_base &C,
+    const string &IsolationString,
+    const string &NName,
+    const string &CName) :
+  transaction_base(C, NName, CName),
+  m_StartCmd()
+{
+  if (IsolationString != isolation_traits<read_committed>::name())
+    m_StartCmd = "SET TRANSACTION ISOLATION LEVEL " + IsolationString;
+}
+
+
+void pqxx::dbtransaction::start_backend_transaction()
+{
+  DirectExec("BEGIN", 2);
+  // TODO: Can't we pipeline this to eliminate roundtrip time?
+  if (!m_StartCmd.empty()) DirectExec(m_StartCmd.c_str());
+}
+
+
+pqxx::result pqxx::dbtransaction::do_exec(const char Query[])
+{
+  try
+  {
+    return DirectExec(Query);
+  }
+  catch (const exception &)
+  {
+    try { abort(); } catch (const exception &) {}
+    throw;
+  }
+}
+
