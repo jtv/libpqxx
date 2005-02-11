f94a8439226533373ff1cedd6f67e19d90692077
Jeroen Vermeulen <jtv@users.noreply.github.com>
2005-02-11 08:17:53 +0000

    Added missing compiler.h include

--- libpqxx/src/dbtransaction.cxx
+++ libpqxx/src/dbtransaction.cxx
@@ -7,7 +7,7 @@
  *      implementation of the pqxx::dbtransaction class.
  *   pqxx::dbtransaction represents a real backend transaction
  *
- * Copyright (c) 2004, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ * Copyright (c) 2004-2005, Jeroen T. Vermeulen <jtv@xs4all.nl>
  *
  * See COPYING for copyright license.  If you did not receive a file called
  * COPYING with this source code, please notify the distributor of this mistake,
@@ -15,6 +15,7 @@
  *
  *-------------------------------------------------------------------------
  */
+#include "pqxx/compiler.h"
 
 #include "pqxx/dbtransaction"
 
