2765c38bb4c7cd247be5a5f42f153321ac2dec35
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2005-04-03 11:27:42 +0000

    Work around division-by-zero error in Visual C++, thanks Marvin Bellamy

--- trunk/libpqxx/config/sample-headers/compiler/VisualC++.NET-2003/config-internal-compiler.h
+++ trunk/libpqxx/config/sample-headers/compiler/VisualC++.NET-2003/config-internal-compiler.h
@@ -2,6 +2,7 @@
 /* #define HAVE_UNISTD_H 1 */
 #define PQXX_HAVE_COUNT_IF 1
 #define PQXX_HAVE_LOCALE 1
+#define PQXX_HAVE_QUIET_NAN 1
 /* #define PQXX_HAVE_SLEEP 1 */
 #define PQXX_HAVE_STRING_CLEAR 1
 /* #define PQXX_HAVE_SYS_SELECT_H 1 */
