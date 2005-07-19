8af5a6888ef3c6219e67fe144ddb36a30ef6e20c
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2005-07-19 10:03:06 +0000

    Test connection reactivation inhibition

---
+++ trunk/libpqxx/test/test086.cxx
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include <pqxx/connection>
+
+using namespace PGSTD;
+using namespace pqxx;
+
+// Test inhibition of connection reactivation
+//
+// Usage: test086
+int main()
+{
+  try
+  {
+    const char Query = "SELECT * from pg_tables";
+
+    connection C;
+
+    nontransaction N1(C, "test86N1");
+    cout << "Some datum: " << N1.exec(Query)[0][0] << endl;
+    N1.commit();
+
+    C.inhibit_reactivation(true);
+    C.deactivate();
+
+    try
+    {
+      nontransaction N2(C, "test86N2");
+      N2.exec(Query);
+    }
+    catch (const broken_connection &e)
+    {
+      cout << "(Expected) " << e.what() << endl;
+    }
+    catch (const exception &)
+    {
+      cerr << "Expected broken_connection, got different exception!" << endl;
+      throw;
+    }
+
+    C.inhibit_reactivation(false);
+    work W(C, "test86W");
+    W.exec(Query);
+    W.commit();
+  }
+  catch (const sql_error &e)
+  {
+    cerr << "SQL error: " << e.what() << endl
+	 << "Query was: " << e.query() << endl;
+  }
+  catch (const exception &e)
+  {
+    cerr << "Exception: " << e.what() << endl;
+}
+
