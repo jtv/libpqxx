ac222ad3fce1fd7fcb8611bcae786ba16b539292
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2005-11-21 16:04:37 +0000

    Test new subtransaction class

---
+++ trunk/libpqxx/test/test088.cxx
@@ -0,0 +1,97 @@
+#include <cassert>
+#include <iostream>
+
+#include <pqxx/pqxx>
+
+using namespace PGSTD;
+using namespace pqxx;
+
+
+// Test program for libpqxx.  Attempt to perform nested queries.
+//
+// Usage: test088
+int main()
+{
+  try
+  {
+    connection C;
+    if (!C.supports(connection_base::cap_nested_transactions))
+    {
+      cout << "Backend version does not support nested transactions. "
+	"Skipping test." <<endl;
+      return 0;
+    }
+
+    // Trivial test: create subtransactions, and commit/abort
+    work T0(C, "T0");
+    cout << T0.exec("SELECT 'T0 starts'")[0][0].c_str() << endl;
+    subtransaction T0a(T0, "T0a");
+    T0a.commit();
+    subtransaction T0b(T0, "T0b");
+    T0b.abort();
+    cout << T0.exec("SELECT 'T0 ends'")[0][0].c_str() << endl;
+    T0.commit();
+
+    // Basic functionality: perform query in subtransaction; abort, continue
+    work T1(C, "T1");
+    cout << T1.exec("SELECT 'T1 starts'")[0][0].c_str() << endl;
+    subtransaction T1a(T1, "T1a");
+      cout << T1a.exec("SELECT '  a'")[0][0].c_str() << endl;
+      T1a.commit();
+    subtransaction T1b(T1, "T1b");
+      cout << T1b.exec("SELECT '  b'")[0][0].c_str() << endl;
+      T1b.abort();
+    subtransaction T1c(T1, "T1c");
+      cout << T1c.exec("SELECT '  c'")[0][0].c_str() << endl;
+      T1c.commit();
+    cout << T1.exec("SELECT 'T1 ends'")[0][0].c_str() << endl;
+    T1.commit();
+
+    // Commit/rollback functionality
+    work T2(C, "T2");
+    const string Table = "test088";
+    T2.exec("CREATE TEMP TABLE " + Table + "(no INTEGER, text VARCHAR)");
+
+    T2.exec("INSERT INTO " + Table + " VALUES(1,'T2')");
+
+    subtransaction T2a(T2, "T2a");
+      T2a.exec("INSERT INTO "+Table+" VALUES(2,'T2a')");
+      T2a.commit();
+    subtransaction T2b(T2, "T2b");
+      T2b.exec("INSERT INTO "+Table+" VALUES(3,'T2b')");
+      T2b.abort();
+    subtransaction T2c(T2, "T2c");
+      T2c.exec("INSERT INTO "+Table+" VALUES(4,'T2c')");
+      T2c.commit();
+    const result R = T2.exec("SELECT * FROM " + Table + " ORDER BY no");
+    for (result::const_iterator i=R.begin(); i!=R.end(); ++i)
+      cout << '\t' << i[0].c_str() << '\t' << i[1].c_str() << endl;
+    if (R.size() != 3)
+      throw logic_error("Expected 3 results, got " + to_string(R.size()));
+    int expected[3] = { 1, 2, 4 };
+    for (result::size_type n=0; n<R.size(); ++n)
+      if (R[n][0].as<int>() != expected[n])
+	throw logic_error("Expected row no. " + to_string(expected[n]) + ", "
+	    "got " + R[n][0].c_str());
+  }
+  catch (const sql_error &e)
+  {
+    cerr << "SQL error: " << e.what() << endl
+         << "Query was: " << e.query() << endl;
+    return 1;
+  }
+  catch (const exception &e)
+  {
+    cerr << "Exception: " << e.what() << endl;
+    return 2;
+  }
+  catch (...)
+  {
+    cerr << "Unhandled exception" << endl;
+    return 100;
+  }
+
+  return 0;
+}
+
+
---
+++ trunk/libpqxx/test/test089.cxx
@@ -0,0 +1,132 @@
+#include <cassert>
+#include <iostream>
+
+#include <pqxx/connection>
+#include <pqxx/subtransaction>
+#include <pqxx/transaction>
+
+using namespace PGSTD;
+using namespace pqxx;
+
+namespace
+{
+void test(connection_base &C, const string &desc)
+{
+  cout << "Testing " << desc << ":" << endl;
+
+  // Trivial test: create subtransactions, and commit/abort
+  work T0(C, "T0");
+  cout << T0.exec("SELECT 'T0 starts'")[0][0].c_str() << endl;
+  subtransaction T0a(T0, "T0a");
+  T0a.commit();
+  subtransaction T0b(T0, "T0b");
+  T0b.abort();
+  cout << T0.exec("SELECT 'T0 ends'")[0][0].c_str() << endl;
+  T0.commit();
+
+  // Basic functionality: perform query in subtransaction; abort, continue
+  work T1(C, "T1");
+  cout << T1.exec("SELECT 'T1 starts'")[0][0].c_str() << endl;
+  subtransaction T1a(T1, "T1a");
+    cout << T1a.exec("SELECT '  a'")[0][0].c_str() << endl;
+    T1a.commit();
+  subtransaction T1b(T1, "T1b");
+    cout << T1b.exec("SELECT '  b'")[0][0].c_str() << endl;
+    T1b.abort();
+  subtransaction T1c(T1, "T1c");
+    cout << T1c.exec("SELECT '  c'")[0][0].c_str() << endl;
+    T1c.commit();
+  cout << T1.exec("SELECT 'T1 ends'")[0][0].c_str() << endl;
+  T1.commit();
+}
+
+bool test_and_catch(connection_base &C, const string &desc)
+{
+  bool ok = false;
+  try
+  {
+    test(C,desc);
+    ok = true;
+  }
+  catch (const broken_connection &)
+  {
+    throw;
+  }
+  catch (const logic_error &)
+  {
+    throw;
+  }
+  catch (const exception &e)
+  {
+    if (C.supports(connection_base::cap_nested_transactions))
+      throw;
+    cout << "Backend does not support nested transactions." << endl;
+  }
+
+  return ok;
+}
+
+} // namespace
+
+// Test program for libpqxx.  Attempt to perform nested queries on various types
+// of connections.
+//
+// Usage: test089
+int main()
+{
+  try
+  {
+    asyncconnection A1;
+    const bool ok = test_and_catch(A1, "asyncconnection (virgin)");
+
+    asyncconnection A2;
+    A2.activate();
+    if (!A2.supports(connection_base::cap_nested_transactions))
+    {
+      if (ok)
+	throw logic_error("Initialized asyncconnection doesn't support "
+	    "nested transactions, but a virgin one does!");
+      cout << "Backend does not support nested transactions.  Skipping test."
+	   << endl;
+      return 0;
+    }
+
+    if (!ok)
+      throw logic_error("Virgin asyncconnection supports nested transactions, "
+	  "but initialized one doesn't!");
+
+    test(A2, "asyncconnection (initialized)");
+
+    lazyconnection L1;
+    test(L1, "lazyconnection (virgin)");
+
+    lazyconnection L2;
+    L2.activate();
+    test(L2, "lazyconnection (initialized)");
+
+    connection C;
+    C.activate();
+    C.deactivate();
+    test(C, "connection (deactivated)");
+  }
+  catch (const sql_error &e)
+  {
+    cerr << "SQL error: " << e.what() << endl
+         << "Query was: " << e.query() << endl;
+    return 1;
+  }
+  catch (const exception &e)
+  {
+    cerr << "Exception: " << e.what() << endl;
+    return 2;
+  }
+  catch (...)
+  {
+    cerr << "Unhandled exception" << endl;
+    return 100;
+  }
+
+  return 0;
+}
+
+
