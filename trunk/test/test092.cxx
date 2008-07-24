22c2e40c23b619f3fe39f56af0e5cdd7da9d4842
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2008-07-24 21:21:11 +0000

    Converted test092 to use test helpers.

--- trunk/test/test092.cxx
+++ trunk/test/test092.cxx
@@ -5,99 +5,83 @@
 
 #include <pqxx/pqxx>
 
+#include "test_helpers.hxx"
+
+
 using namespace PGSTD;
 using namespace pqxx;
 
 // Test program for libpqxx.  Test binary parameters to prepared statements.
-//
-// Usage: test092
-int main()
+
+namespace
 {
-  try
+void test_092(connection_base &C, transaction_base &T)
+{
+  const char databuf[] = "Test\0data";
+  const string data(databuf, sizeof(databuf));
+  assert(data.size() > strlen(databuf));
+
+  const string Table = "pqxxbin", Field = "binfield", Stat = "nully";
+  T.exec("CREATE TEMP TABLE " + Table + " (" + Field + " BYTEA)");
+
+  if (!C.supports(connection_base::cap_prepared_statements))
   {
-    const char databuf[] = "Test\0data";
-    const string data(databuf, sizeof(databuf));
-    assert(data.size() > strlen(databuf));
+    cout << "Backend version does not support prepared statements.  Skipping."
+         << endl;
+    return;
+  }
 
-    const string Table = "pqxxbin", Field = "binfield", Stat = "nully";
-
-    lazyconnection C;
-    work T(C, "test92");
-    T.exec("CREATE TEMP TABLE " + Table + " (" + Field + " BYTEA)");
-
-    if (!C.supports(connection_base::cap_prepared_statements))
-    {
-      cout << "Backend version does not support prepared statements.  Skipping."
-           << endl;
-      return 0;
-    }
-
-    C.prepare(Stat, "INSERT INTO " + Table + " VALUES ($1)")
+  C.prepare(Stat, "INSERT INTO " + Table + " VALUES ($1)")
 	("BYTEA", pqxx::prepare::treat_binary);
-    T.prepared(Stat)(data).exec();
+  T.prepared(Stat)(data).exec();
 
-    const result L( T.exec("SELECT length("+Field+") FROM " + Table) );
-    if (L[0][0].as<size_t>() != data.size())
-      throw logic_error("Inserted " + to_string(data.size()) + " bytes, "
-	"but " + L[0][0].as<string>() + " arrived");
+  const result L( T.exec("SELECT length("+Field+") FROM " + Table) );
+  PQXX_CHECK_EQUAL(L[0][0].as<size_t>(),
+	data.size(),
+	"Length of field in database does not match original length.")
 
-    const result R( T.exec("SELECT " + Field + " FROM " + Table) );
+  const result R( T.exec("SELECT " + Field + " FROM " + Table) );
 
-    const binarystring roundtrip(R[0][0]);
+  const binarystring roundtrip(R[0][0]);
 
-    if (string(roundtrip.str()) != data)
-      throw logic_error("Sent " + to_string(data.size()) + " bytes "
-	"of binary data, got " + to_string(roundtrip.size()) + " back: "
-	"'" + roundtrip.str() + "'");
+  PQXX_CHECK_EQUAL(string(roundtrip.str()), data, "Data came back different.")
 
-    if (roundtrip.size() != data.size())
-      throw logic_error("Binary string reports wrong size: " +
-	to_string(roundtrip.size()) + " "
-	"(expected " + to_string(data.size()) + ")");
+  PQXX_CHECK_EQUAL(roundtrip.size(),
+	data.size(),
+	"Binary string reports wrong size.")
 
-    // People seem to like the multi-line invocation style, where you get your
-    // invocation object first, then add parameters in separate C++ statements.
-    // As John Mudd found, that used to break the code.  Let's test it.
-    T.exec("CREATE TEMP TABLE tuple (one INTEGER, two VARCHAR)");
+  // People seem to like the multi-line invocation style, where you get your
+  // invocation object first, then add parameters in separate C++ statements.
+  // As John Mudd found, that used to break the code.  Let's test it.
+  T.exec("CREATE TEMP TABLE tuple (one INTEGER, two VARCHAR)");
 
-    pqxx::prepare::declaration d(
+  pqxx::prepare::declaration d(
 	C.prepare("maketuple", "INSERT INTO tuple VALUES ($1, $2)") );
-    d("INTEGER", pqxx::prepare::treat_direct);
-    d("VARCHAR", pqxx::prepare::treat_string);
+  d("INTEGER", pqxx::prepare::treat_direct);
+  d("VARCHAR", pqxx::prepare::treat_string);
 
-    pqxx::prepare::invocation i( T.prepared("maketuple") );
-    const string f = "frobnalicious";
-    i(6);
-    i(f);
-    i.exec();
+  pqxx::prepare::invocation i( T.prepared("maketuple") );
+  const string f = "frobnalicious";
+  i(6);
+  i(f);
+  i.exec();
 
-    const result t( T.exec("SELECT * FROM tuple") );
-    if (t.size() != 1)
-      throw logic_error("Expected 1 tuple, got " + to_string(t.size()));
-    if (t[0][0].as<string>() != "6")
-      throw logic_error("Expected value 6, got " + t[0][0].as<string>());
-    if (t[0][1].c_str() != f)
-      throw logic_error("Expected string '" + f + "', "
+  const result t( T.exec("SELECT * FROM tuple") );
+  if (t.size() != 1)
+    throw logic_error("Expected 1 tuple, got " + to_string(t.size()));
+  if (t[0][0].as<string>() != "6")
+    throw logic_error("Expected value 6, got " + t[0][0].as<string>());
+  if (t[0][1].c_str() != f)
+    throw logic_error("Expected string '" + f + "', "
 	"got '" + t[0][1].c_str() + "'");
-  }
-  catch (const sql_error &e)
-  {
-    cerr << "SQL error: " << e.what() << endl
-         << "Query was: " << e.query() << endl;
-    return 1;
-  }
-  catch (const exception &e)
-  {
-    cerr << "Exception: " << e.what() << endl;
-    return 2;
-  }
-  catch (...)
-  {
-    cerr << "Unhandled exception" << endl;
-    return 100;
-  }
-
-  return 0;
 }
 
+} // namespace
+
+
+int main()
+{
+  test::TestCase<lazyconnection> test092("test_092", test_092);
+  return test::pqxxtest(test092);
+}
 
