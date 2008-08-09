13bb0df1f5999ad9582d5c4848e194fbc47ed05b
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2008-08-09 03:56:23 +0000

    Converted test008 to test framework.

--- trunk/test/test008.cxx
+++ trunk/test/test008.cxx
@@ -6,6 +6,8 @@
 #include <pqxx/tablereader>
 #include <pqxx/transaction>
 
+#include "test_helpers.hxx"
+
 using namespace PGSTD;
 using namespace pqxx;
 
@@ -13,86 +15,54 @@ using namespace pqxx;
 // Simple test program for libpqxx.  Read a table using a tablereader, which
 // may be faster than a conventional query.  A tablereader is really a frontend
 // for a PostgreSQL COPY TO stdout command.
-//
-// Usage: test008 [connect-string] [table]
-//
-// Where connect-string is a set of connection options in Postgresql's
-// PQconnectdb() format, eg. "dbname=template1" to select from a database
-// called template1, or "host=foo.bar.net user=smith" to connect to a backend
-// running on host foo.bar.net, logging in as user smith.
-//
-// The default table name is "pqxxevents" as used by other test programs.
-// PostgreSQL currently implements pg_tables as a view, which cannot be read by
-// using the COPY command.  Otherwise, pg_tables would have made a better
-// default value here.
-int main(int argc, char *argv[])
+
+namespace
 {
-  try
+void test_008(connection_base &, transaction_base &T)
+{
+  const string Table = "pqxxevents";
+
+  vector<string> R, First;
+
+  // Set up a tablereader stream to read data from table pg_tables
+  tablereader Stream(T, Table);
+
+  // Read results into string vectors and print them
+  for (int n=0; (Stream >> R); ++n)
   {
-    // Set up a connection to the backend
-    connection C(argv[1]);
+    // Keep the first row for later consistency check
+    if (n == 0) First = R;
 
-    string Table = "pqxxevents";
-    if (argc > 2) Table = argv[2];
-
-    // Begin a transaction acting on our current connection
-    work T(C, "test8");
-
-    vector<string> R, First;
-
-    // Set up a tablereader stream to read data from table pg_tables
-    tablereader Stream(T, Table);
-
-    // Read results into string vectors and print them
-    for (int n=0; (Stream >> R); ++n)
-    {
-      // Keep the first row for later consistency check
-      if (n == 0) First = R;
-
-      cout << n << ":\t" << separated_list("\t",R.begin(),R.end()) << endl;
-      R.clear();
-    }
-
-    Stream.complete();
-
-    // Verify the contents we got for the first row
-    if (!First.empty())
-    {
-      tablereader Verify(T, Table);
-      string Line;
-
-      if (!Verify.get_raw_line(Line))
-	throw logic_error("tablereader got rows the first time around, "
-	                  "but none the second time!");
-
-      cout << "First tuple was: " << endl << Line << endl;
-
-      Verify.tokenize(Line, R);
-      if (R != First)
-        throw logic_error("Got different results re-parsing first tuple!");
-    }
-  }
-  catch (const sql_error &e)
-  {
-    // If we're interested in the text of a failed query, we can write separate
-    // exception handling code for this type of exception
-    cerr << "SQL error: " << e.what() << endl
-         << "Query was: '" << e.query() << "'" << endl;
-    return 1;
-  }
-  catch (const exception &e)
-  {
-    // All exceptions thrown by libpqxx are derived from std::exception
-    cerr << "Exception: " << e.what() << endl;
-    return 2;
-  }
-  catch (...)
-  {
-    // This is really unexpected (see above)
-    cerr << "Unhandled exception" << endl;
-    return 100;
+    cout << n << ":\t" << separated_list("\t",R.begin(),R.end()) << endl;
+    R.clear();
   }
 
-  return 0;
+  Stream.complete();
+
+  // Verify the contents we got for the first row
+  if (!First.empty())
+  {
+    tablereader Verify(T, Table);
+    string Line;
+
+    const bool outcome(Verify.get_raw_line(Line));
+    PQXX_CHECK(
+	outcome,
+	"tablereader got rows the first time around, but not the second time.");
+
+    cout << "First tuple was: " << endl << Line << endl;
+
+    Verify.tokenize(Line, R);
+    PQXX_CHECK_EQUAL(R, First, "Got different results re-parsing first tuple.");
+  }
+}
+
+} // namespace
+
+
+int main()
+{
+  test::TestCase<> test008("test_008", test_008);
+  return test::pqxxtest(test008);
 }
 
