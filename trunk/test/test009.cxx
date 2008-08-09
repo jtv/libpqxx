c49499324588936ce2fa2abb4974f9a404450309
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2008-08-09 04:32:11 +0000

    Converted test 009 to test framework.

--- trunk/test/test009.cxx
+++ trunk/test/test009.cxx
@@ -7,21 +7,14 @@
 #include <pqxx/tablewriter>
 #include <pqxx/transaction>
 
+#include "test_helpers.hxx"
+
 using namespace PGSTD;
 using namespace pqxx;
 
 
 // Test program for libpqxx.  Create a table and write data to it, using
 // tablewriter's back_insert_iterator.
-//
-// Usage: test009 [connect-string] [table] [column]
-//
-// Where the connect-string is a set of connection options in Postgresql's
-// PQconnectdb() format, eg. "dbname=template1" to select from a database
-// called template1, or "host=foo.bar.net user=smith" to connect to a backend
-// running on host foo.bar.net, logging in as user smith.
-//
-// The default table name is "testtable."
 
 namespace
 {
@@ -70,73 +63,44 @@ void CheckTable(transaction_base &T, string TableName)
   result Count = T.exec(("SELECT COUNT(*) FROM " + TableName).c_str());
   size_t Rows = 0;
 
-  if (!Count[0][0].to(Rows)) throw runtime_error("NULL row count!");
+  const bool have_rowcount = Count[0][0].to(Rows);
+  PQXX_CHECK(have_rowcount, "NULL row count.");
   cout << Rows << " rows in table." << endl;
 
-  if (Rows != Contents.size())
-    throw runtime_error("Found " +
-		        string(Count[0][0].c_str()) +
-			" rows in table--after writing " +
-			to_string(Contents.size()) +
-			"!");
-
+  PQXX_CHECK_EQUAL(
+	Rows,
+	Contents.size(),
+	"Number of rows in table is not what I wrote.");
   // TODO: Compare table contents to Contents
 }
+
+void test_009(connection_base &, transaction_base &T)
+{
+  PrepareContents();
+
+  // Select our original and destination table names
+  const string TableName = "pqxxtesttable";
+
+  const string Column = "content";
+
+  // Create table.  If the table already existed, better to fail now.
+  stringstream ctq;
+  ctq << "CREATE TABLE " << TableName << '(' << Column << " VARCHAR)";
+  T.exec(ctq);
+
+  FillTable(T, TableName, Column);
+  CheckTable(T, TableName);
+
+  T.exec(("DROP TABLE " + TableName).c_str());
+  T.commit();
 }
 
-int main(int argc, char *argv[])
+} // namespace
+
+
+int main()
 {
-  try
-  {
-    const char *ConnStr = argv[1];
-
-    PrepareContents();
-
-    // Set up two connections to the backend: one to read our original table,
-    // and another to write our copy
-    connection C(ConnStr);
-
-    // Select our original and destination table names
-    string TableName = "pqxxtesttable";
-    if (argc > 2) TableName = argv[2];
-
-    string Column = "content";
-    if (argc > 3) Column = argv[3];
-
-    work T(C, "test9");
-
-    // Create table.  If the table already existed, better to fail now.
-    stringstream ctq;
-    ctq << "CREATE TABLE " << TableName << '(' << Column << " VARCHAR)";
-    T.exec(ctq);
-
-    FillTable(T, TableName, Column);
-    CheckTable(T, TableName);
-
-    T.exec(("DROP TABLE " + TableName).c_str());
-    T.commit();
-  }
-  catch (const sql_error &e)
-  {
-    // If we're interested in the text of a failed query, we can write separate
-    // exception handling code for this type of exception
-    cerr << "SQL error: " << e.what() << endl
-         << "Query was: '" << e.query() << "'" << endl;
-    return 1;
-  }
-  catch (const exception &e)
-  {
-    // All exceptions thrown by libpqxx are derived from std::exception
-    cerr << "Exception: " << e.what() << endl;
-    return 2;
-  }
-  catch (...)
-  {
-    // This is really unexpected (see above)
-    cerr << "Unhandled exception" << endl;
-    return 100;
-  }
-
-  return 0;
+  test::TestCase<> test009("test_009", test_009);
+  return test::pqxxtest(test009);
 }
 
