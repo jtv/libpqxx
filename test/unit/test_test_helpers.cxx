7045e0fa6e604246bc80c50e4b8a52be387bf966
Jeroen Vermeulen <jtv@users.noreply.github.com>
2008-08-10 13:38:56 +0000

    More work on test helpers.

--- test/unit/test_test_helpers.cxx
+++ test/unit/test_test_helpers.cxx
@@ -5,109 +5,147 @@ using namespace pqxx;
 using namespace pqxx::test;
 
 
+namespace
+{
 void empty() {}
 
-void test_exception()
+void test_test_helpers(connection_base &, transaction_base &)
 {
+  bool failed = true;
+
+  // At a minimum, PQXX_CHECK_NOTREACHED must work.
+  try
+  {
+    PQXX_CHECK_NOTREACHED("(expected)");
+    failed = false;
+  }
+  catch (const test_failure &)
+  {
+    // This is what we expect.
+  }
+  if (!failed)
+    throw test_failure(__FILE__, __LINE__, "PQXX_CHECK_NOTREACHED is broken.");
+
+  // Another very basic test macro is PQXX_CHECK.
+  PQXX_CHECK(true, "PQXX_CHECK is broken.");
+
+  failed = true;
+  try
+  {
+    PQXX_CHECK(false, "(expected)");
+    failed = false;
+  }
+  catch (const test_failure &)
+  {
+  }
+  if (!failed) PQXX_CHECK_NOTREACHED("PQXX_CHECK failed to notice failure.");
+
+  // PQXX_CHECK_THROWS is more complicated.
+  // It can handle a std::exception...
   PQXX_CHECK_THROWS(
 	throw exception(),
 	exception,
 	"Plain exception not handled properly by PQXX_CHECK_THROWS.");
+
+  // ...or any other type of exception.
+  PQXX_CHECK_THROWS(
+	throw test_failure(__FILE__, __LINE__, "(expected)"),
+	 test_failure,
+	"PQXX_CHECK_THROWS() failed to catch expected exception.");
+
+  // Even if it's not std::exception-derived.
+  PQXX_CHECK_THROWS(throw 1, int, "(expected)");
+
+  // PQXX_CHECK_THROWS means there _must_ be an exception.
+  failed = true;
+  try
+  {
+    // If the test fails to throw, PQXX_CHECK_THROWS throws a failure.
+    PQXX_CHECK_THROWS(empty(), exception, "(expected)");
+    // So we shouldn't get to this point.
+    failed = false;
+  }
+  catch (const test_failure &)
+  {
+    // Instead, we go straight here.
+  }
+  PQXX_CHECK(failed, "PQXX_CHECK_THROWS did not notice missing exception.");
+
+  // The exception must be of the right type (or a subclass of the right type).
+  failed = true;
+  try
+  {
+    // If the test throws the wrong type, PQXX_CHECK_THROWS throws a failure.
+    PQXX_CHECK_THROWS(throw exception(), test_failure, "(expected)");
+    failed = false;
+  }
+  catch (const test_failure &)
+  {
+    // Instead, we go straight here.
+  }
+  PQXX_CHECK(failed, "PQXX_CHECK_THROWS did not notice wrong exception type.");
+
+  // PQXX_CHECK_THROWS can test itself...
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK_THROWS(empty(), test_failure, "(expected)"),
+	test_failure,
+	"PQXX_CHECK_THROWS failed to throw for missing exception.");
+
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK_THROWS(throw 1, exception, "(expected)"),
+	test_failure,
+	"PQXX_CHECK_THROWS failed to throw for wrong exception type.");
+
+  // ...and other test helpers.
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK_NOTREACHED("(expected)"),
+	test_failure,
+	"PQXX_CHECK_THROWS did not catch PQXX_CHECK_NOTREACHED.");
+
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK(false, "(expected)"),
+	test_failure,
+	"PQXX_CHECK_THROWS did not catch failing PQXX_CHECK.");
+
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK_THROWS(
+		PQXX_CHECK(true, "(shouldn't happen)"),
+		test_failure,
+		"(expected)"),
+	test_failure,
+	"PQXX_CHECK_THROWS on successful PQXX_CHECK failed to throw.");
+
+  // PQXX_CHECK_EQUAL tests for equality.  Its arguments need not be of the same
+  // type, as long as equality between them is defined.
+  PQXX_CHECK_EQUAL(1, 1, "PQXX_CHECK_EQUAL is broken.");
+  PQXX_CHECK_EQUAL(1, 1L, "PQXX_CHECK_EQUAL breaks on type mismatch.");
+
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK_EQUAL(1, 2, "(expected)"),
+	test_failure,
+	"PQXX_CHECK_EQUAL fails to spot inequality.");
+
+  // PQXX_CHECK_NOT_EQUAL is like PQXX_CHECK_EQUAL, but tests for inequality.
+  PQXX_CHECK_NOT_EQUAL(1, 2, "PQXX_CHECK_NOT_EQUAL is broken.");
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK_NOT_EQUAL(1, 1, "(expected)"),
+	test_failure,
+	"PQXX_CHECK_NOT_EQUAL fails to fail when arguments are equal.");
+  PQXX_CHECK_THROWS(
+	PQXX_CHECK_NOT_EQUAL(1, 1L, "(expected)"),
+	test_failure,
+	"PQXX_CHECK_NOT_EQUAL breaks on type mismatch.");
 }
 
-void test_simple_failure()
-{
-  throw test_failure(__FILE__, __LINE__, "(expected) Testing test failure.");
-}
 
-void test_check_throws_correct()
-{
-  PQXX_CHECK_THROWS(test_simple_failure(), test_failure,
-	"PQXX_CHECK_THROWS() failed");
-}
-
-void test_check_throws_nothrow()
-{
-  PQXX_CHECK_THROWS(empty(), test_failure, "(expected)");
-}
-
-void test_check_throws_nothrow_meta()
-{
-  PQXX_CHECK_THROWS(test_check_throws_nothrow(), test_failure, "(expected)");
-}
-
-void test_check_throws_unexpected()
-{
-  PQXX_CHECK_THROWS(test_simple_failure(), int, "(expected)");
-}
-
-void test_check_throws_unexpected_meta()
-{
-  PQXX_CHECK_THROWS(test_check_throws_unexpected(), test_failure, "(expected)");
-}
+} // namespace
 
 
 int main()
 {
-  int tr;
-
-  // Check normal case: passing test.
-  tr = pqxxtest(empty);
-  if (tr != 0)
-  {
-    cerr << "Empty test failed: "
-	"expected return value 0, got " << to_string(tr) << endl;
-    return 1;
-  }
-
-  // Check that pqxxtest() handles and reports failures.
-  tr = pqxxtest(test_simple_failure);
-  if (tr != 1)
-  {
-    cerr << "Test for test failure failed: "
-	"expected return value 1, got " << to_string(tr) << endl;
-    return tr;
-  }
-
-  // Now we know that pqxxtest() works, use it to test PQXX_CHECK_THROWS().
-  tr = pqxxtest(test_check_throws_correct);
-  if (tr != 0)
-  {
-    cerr << "PQXX_CHECK_THROWS() failed 'correct' case: "
-	"expected return value 0, got " << to_string(tr) << endl;
-  }
-
-  // Check that PQXX_CHECK_THROWS() fails if action does not throw.
-  tr = pqxxtest(test_check_throws_nothrow);
-  if (tr != 1)
-  {
-    cerr << "PQXX_CHECK_THROWS() failed 'no throw' case: "
-	"expected return value 1, got " << to_string(tr) << endl;
-  }
-
-  // Check PQXX_CHECK_THROWS() on itself for case where action does not throw.
-  tr = pqxxtest(test_check_throws_nothrow_meta);
-  if (tr != 0)
-  {
-    cerr << "PQXX_CHECK_THROWS() failed to throw test_failure for 'no throw' "
-	"case: expected return value 0, got " << to_string(tr) << endl;
-  }
-
-  // Check that PQXX_CHECK_THROWS() fails if action throws the wrong type.
-  tr = pqxxtest(test_check_throws_unexpected);
-  if (tr != 1)
-  {
-    cerr << "PQXX_CHECK_THROWS() failed 'unexpected' case: "
-	"expected return value 1, got " << to_string(tr) << endl;
-  }
-
-  // Check PQXX_CHECK_THROWS() on itself for case where action does not throw.
-  tr = pqxxtest(test_check_throws_unexpected_meta);
-  if (tr != 0)
-  {
-    cerr << "PQXX_CHECK_THROWS() failed to throw test_failure for 'unexpected' "
-	"case: expected return value 0, got " << to_string(tr) << endl;
-  }
-
+  TestCase<nullconnection, nontransaction> test(
+	"test_test_helpers",
+	test_test_helpers);
+  return test.run();
 }
 
