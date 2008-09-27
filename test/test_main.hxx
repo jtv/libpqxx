b03f915e2e5bc7efe80419690853d02722ac0840
Jeroen Vermeulen <jtv@users.noreply.github.com>
2008-09-27 13:48:56 +0000

    Test runners check for failure.

--- test/test_main.hxx
+++ test/test_main.hxx
@@ -1,42 +1,38 @@
-using namespace pqxx;
-using namespace pqxx::test;
+/* main() definition for libpqxx test runners.
+ */
 
-namespace
-{
-void run_tests(const char test_name[])
-{
-  const test_map &tests = register_test(NULL);
-  int test_count = 0;
-  for (test_map::const_iterator i = tests.begin(); i != tests.end(); ++i)
-    if (!test_name || test_name == i->first)
-    {
-      PGSTD::cout << PGSTD::endl << "Running: " << i->first << PGSTD::endl;
-      i->second->run();
-      ++test_count;
-    }
-  PGSTD::cout << "Ran " << test_count << " test(s)." << PGSTD::endl;
-}
-}
+pqxx::test::base_test::~base_test() {}
 
 
-namespace pqxx
-{
-namespace test
-{
-base_test::~base_test() {}
-
-const test_map &register_test(base_test *tc)
+const pqxx::test::test_map &pqxx::test::register_test(base_test *tc)
 {
   static test_map tests;
   if (tc) tests[tc->name()] = tc;
   return tests;
 }
-}
-}
 
 
 int main(int, char *argv[])
 {
-  run_tests(argv[1]);
-}
+  const char *const test_name = argv[1];
+  const pqxx::test::test_map &tests = pqxx::test::register_test(NULL);
 
+  int test_count = 0, failures = 0;
+  for (pqxx::test::test_map::const_iterator i = tests.begin();
+       i != tests.end();
+       ++i)
+    if (!test_name || test_name == i->first)
+    {
+      PGSTD::cout << PGSTD::endl << "Running: " << i->first << PGSTD::endl;
+      const int outcome = i->second->run();
+      if (outcome != 0) ++failures;
+      ++test_count;
+    }
+
+  PGSTD::cout << "Ran " << test_count << " test(s)." << PGSTD::endl;
+
+  if (failures > 0)
+    PGSTD::cout << "*** " << failures << " test(s) failed. ***" << PGSTD::endl;
+
+  return failures;
+}
