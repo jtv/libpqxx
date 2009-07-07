6b6320c3df1e25610c9356896a1207d9a792547e
Jeroen Vermeulen <jtv@users.noreply.github.com>
2009-07-07 09:54:47 +0000

    Version shared libraries based on ABI, not full version number.

--- autogen.sh
+++ autogen.sh
@@ -26,17 +26,22 @@ echo "libpqxx version $PQXXVERSION"
 PQXX_ABI=`./tools/extract_version --abi`
 echo "libpqxx ABI version $PQXX_ABI"
 
-# Generate configure.ac based on current version numbers
-sed -e "s/@PQXXVERSION@/$PQXXVERSION/g" configure.ac.in >configure.ac
-
-# Generate version header.
 PQXX_MAJOR="`echo "$PQXXVERSION" | sed -e 's/[[:space:]]*\([0-9]*\)\..*$/\1/'`"
 PQXX_MINOR="`echo "$PQXXVERSION" | sed -e 's/[^.]*\.\([0-9]*\).*$/\1/'`"
-sed -e "s/@PQXXVERSION@/$PQXXVERSION/g" \
-	-e "s/@PQXX_MAJOR@/$PQXX_MAJOR/g" \
-	-e "s/@PQXX_MINOR@/$PQXX_MINOR/g" \
-	-e "s/@PQXX_ABI@/$PQXX_ABI/g" \
-	include/pqxx/version.hxx.template >include/pqxx/version.hxx
+
+substitute() {
+	sed -e "s/@PQXXVERSION@/$PQXXVERSION/g" \
+		-e "s/@PQXX_MAJOR@/$PQXX_MAJOR/g" \
+		-e "s/@PQXX_MINOR@/$PQXX_MINOR/g" \
+		-e "s/@PQXX_ABI@/$PQXX_ABI/g" \
+		"$1"
+}
+
+# Generate configure.ac based on current version numbers
+substitute configure.ac.in >configure.ac
+
+# Generate version header.
+substitute include/pqxx/version.hxx.template >include/pqxx/version.hxx
 
 # Generate Windows makefiles (adding carriage returns to make it MS-DOS format)
 makewinmake() {
