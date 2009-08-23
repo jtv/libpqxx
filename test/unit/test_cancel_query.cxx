994de562465ea0c553758e981acc912263ac6532
Jeroen Vermeulen <jtv@users.noreply.github.com>
2009-08-23 13:18:58 +0000

    The old PQcancelRequest fails the cancel test.

--- test/unit/test_cancel_query.cxx
+++ test/unit/test_cancel_query.cxx
@@ -1,3 +1,5 @@
+#include <pqxx/compiler-internal.hxx>
+
 #include <test_helpers.hxx>
 
 using namespace PGSTD;
@@ -11,7 +13,9 @@ void test_cancel_query(connection_base &c, transaction_base &t)
   p.retain(0);
   const pipeline::query_id i = p.insert("SELECT pg_sleep(3)");
   c.cancel_query();
+#ifdef PQXX_HAVE_PQCANCEL
   PQXX_CHECK_THROWS(p.retrieve(i), sql_error, "Canceled query did not throw.");
+#endif
 }
 } // namespace
 
