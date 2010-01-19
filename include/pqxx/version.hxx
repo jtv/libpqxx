198f851c4672d8b4130b3cb56409348668e3a025
Jeroen Vermeulen <jtv@users.noreply.github.com>
2010-01-19 03:31:59 +0000

    Generated file, but needed in revision control to enable ./configure ; make

---
+++ include/pqxx/version.hxx
@@ -0,0 +1,32 @@
+/*-------------------------------------------------------------------------
+ *
+ *   FILE
+ *	pqxx/version.hxx
+ *
+ *   DESCRIPTION
+ *      libpqxx version description.
+ *   DO NOT INCLUDE THIS FILE DIRECTLY; include pqxx/version instead.
+ *
+ * Copyright (c) 2009, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ *
+ * See COPYING for copyright license.  If you did not receive a file called
+ * COPYING with this source code, please notify the distributor of this mistake,
+ * or contact the author.
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef PQXX_H_VERSION
+
+/// Full libpqxx version string.
+#define PQXX_VERSION "3.1"
+
+/// Major version number for this libpqxx release.
+#define PQXX_VERSION_MAJOR 3
+
+/// Minor version number for this libpqxx release.
+#define PQXX_VERSION_MINOR 1
+
+/// Library ABI version.
+#define PQXX_ABI "3.1"
+
+#endif
