246211c302168670f6ae2e5e469b0c5da2483cec
Jeroen Vermeulen <jtv@users.noreply.github.com>
2010-01-24 17:02:48 +0000

    More work on getting requirements for ./configure into the repo.

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ set -e
 #export LC_ALL=C
 
 latest_automake() {
-	for v in "-1.10" "-1.9" "-1.8" "-1.7" "-1.6" "" ; do
+	for v in "-1.11" "-1.10" "-1.9" "-1.8" "-1.7" "-1.6" "" ; do
 		if which "automake$v" >/dev/null ; then
 			echo "$v"
 			return
