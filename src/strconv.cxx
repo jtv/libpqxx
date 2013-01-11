3a5d58f0b2f32a6374619c18fe22ed62c81929d2
Jeroen Vermeulen <jtv@users.noreply.github.com>
2013-01-11 18:08:06 +0000

    Copyright update.

--- src/strconv.cxx
+++ src/strconv.cxx
@@ -6,7 +6,7 @@
  *   DESCRIPTION
  *      implementation of string conversions
  *
- * Copyright (c) 2008-2012, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ * Copyright (c) 2008-2013, Jeroen T. Vermeulen <jtv@xs4all.nl>
  *
  * See COPYING for copyright license.  If you did not receive a file called
  * COPYING with this source code, please notify the distributor of this mistake,
