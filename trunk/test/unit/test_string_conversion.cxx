d5b44a2e67a9c70f4d0c8be6bbd37e3ad47f3b26
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2013-01-11 18:04:35 +0000

    Some integer overflows were not detected in from_string.  Fixes #263.

--- trunk/test/unit/test_string_conversion.cxx
+++ trunk/test/unit/test_string_conversion.cxx
@@ -29,6 +29,37 @@ void test_string_conversion(transaction_base &)
 	"C++ string",
 	to_string(cxx_string),
 	"C++-style string object does not convert to string properly.");
+
+  PQXX_CHECK_EQUAL("0", to_string(0), "Zero does not convert right.");
+  PQXX_CHECK_EQUAL("1", to_string(1), "Basic integer does not convert right.");
+  PQXX_CHECK_EQUAL("-1", to_string(-1), "Negative numbers don't work.");
+  PQXX_CHECK_EQUAL("9999", to_string(9999), "Larger numbers don't work.");
+  PQXX_CHECK_EQUAL(
+	"-9999",
+	to_string(-9999),
+	"Larger negative numbers don't work.");
+
+  int x;
+  from_string("0", x);
+  PQXX_CHECK_EQUAL(0, x, "Zero does not parse right.");
+  from_string("1", x);
+  PQXX_CHECK_EQUAL(1, x, "Basic integer does not parse right.");
+  from_string("-1", x);
+  PQXX_CHECK_EQUAL(-1, x, "Negative numbers don't work.");
+  from_string("9999", x);
+  PQXX_CHECK_EQUAL(9999, x, "Larger numbers don't work.");
+  from_string("-9999", x);
+  PQXX_CHECK_EQUAL(-9999, x, "Larger negative numbers don't work.");
+
+  // Bug #263 describes a case where this kind of overflow went undetected.
+  if (sizeof(unsigned int) == 4)
+  {
+    unsigned int u;
+    PQXX_CHECK_THROWS(
+	from_string("4772185884", u),
+	pqxx::failure,
+	"Overflow not detected.");
+  }
 }
 }
 
