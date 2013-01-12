852d263a3a86a92b640a09a9f9263ba08c4b99f4
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2013-01-12 10:06:51 +0000

    Isolation levels: documentation update.

--- trunk/include/pqxx/isolation.hxx
+++ trunk/include/pqxx/isolation.hxx
@@ -8,7 +8,7 @@
  *   Policies and traits describing SQL transaction isolation levels
  *   DO NOT INCLUDE THIS FILE DIRECTLY; include pqxx/isolation instead.
  *
- * Copyright (c) 2003-2012, Jeroen T. Vermeulen <jtv@xs4all.nl>
+ * Copyright (c) 2003-2013, Jeroen T. Vermeulen <jtv@xs4all.nl>
  *
  * See COPYING for copyright license.  If you did not receive a file called
  * COPYING with this source code, please notify the distributor of this mistake,
@@ -27,14 +27,35 @@
 namespace pqxx
 {
 
-/// Transaction isolation levels; PostgreSQL doesn't implement all SQL levels
-/** The only levels implemented in postgres are read_committed,
- * repeatable_read and serializable; SQL also defines
- * read_uncommitted.  Unless you're bent on using nasty tricks to
- * communicate between ongoing transactions and such, you won't really
- * need isolation levels for anything except performance optimization.
- * In that case, you can safely emulate read_uncommitted by using
- * read_committed.  In general, serializable is the safest choice.
+/// Transaction isolation levels.
+/** These are as defined in the SQL standard.  But there are a few notes
+ * specific to PostgreSQL.
+ *
+ * First, postgres does not support "read uncommitted."  The lowest level you
+ * can get is "read committed," which is better.  PostgreSQL is built on the
+ * MVCC paradigm, which guarantees "read committed" isolation without any
+ * additional performance overhead, so there was no point in providing the
+ * lower level.
+ *
+ * Second, "repeatable read" also makes more isolation guarantees than the
+ * standard requires.  According to the standard, this level prevents "dirty
+ * reads" and "nonrepeatable reads," but not "phantom reads."  In postgres,
+ * it actually prevents all three.
+ *
+ * Third, "serializable" is only properly supported starting at postgres 9.1.
+ * If you request "serializable" isolation on an older backend, you will get
+ * the same isolation as in "repeatable read."  It's better than the "repeatable
+ * read" defined in the SQL standard, but not a complete implementation of the
+ * standard's "serializable" isolation level.
+ *
+ * In general, a lower isolation level will allow more surprising interactions
+ * between ongoing transactions, but improve performance.  A higher level
+ * gives you more protection from subtle concurrency bugs, but sometimes it
+ * may not be possible to complete your transaction without avoiding paradoxes
+ * in the data.  In that case a transaction may fail, and the application will
+ * have to re-do the whole thing based on the latest state of the database.
+ *
+ * Study the levels and design your application with the right level in mind.
  */
 enum isolation_level
 {
