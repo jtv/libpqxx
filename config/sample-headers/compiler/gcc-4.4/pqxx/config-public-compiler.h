4420b64499dcfc23b8c65cc02050925f4bfe4f21
Jeroen Vermeulen <jtv@users.noreply.github.com>
2015-01-14 20:23:05 +0000

    Remove support for g++ 4.2, add sample headers for g++ 4.4.

---
+++ config/sample-headers/compiler/gcc-4.4/pqxx/config-public-compiler.h
@@ -0,0 +1,9 @@
+/* Automatically generated from config.h: public/compiler config. */
+#define PQXX_HAVE_AUTO_PTR 1
+#define PQXX_HAVE_CPP_PRAGMA_MESSAGE 1
+#define PQXX_HAVE_GCC_CONST 1
+#define PQXX_HAVE_GCC_DEPRECATED 1
+#define PQXX_HAVE_GCC_NORETURN 1
+#define PQXX_HAVE_GCC_PURE 1
+#define PQXX_HAVE_LONG_DOUBLE 1
+#define PQXX_HAVE_LONG_LONG 1
