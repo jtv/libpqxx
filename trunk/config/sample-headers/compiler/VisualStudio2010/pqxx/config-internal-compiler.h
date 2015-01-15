0a5fed235b9d821950e4e2335f324b63cad48baf
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2015-01-15 09:43:57 +0000

    Complete check for isinf(), and rename PQXX_HAVE_C_ISINF just like PQXX_HAVE_C_ISNAN before.

--- trunk/config/sample-headers/compiler/VisualStudio2010/pqxx/config-internal-compiler.h
+++ trunk/config/sample-headers/compiler/VisualStudio2010/pqxx/config-internal-compiler.h
@@ -1,5 +1,6 @@
 #define HAVE_SYS_TYPES_H 1
 /* #define HAVE_UNISTD_H 1 */
+/* #define PQXX_HAVE_ISINF 1 */
 /* #define PQXX_HAVE_ISNAN 1 */
 /* #define PQXX_HAVE_SLEEP 1 */
 /* #define PQXX_HAVE_SYS_SELECT_H 1 */
