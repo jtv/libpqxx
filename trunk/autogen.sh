5916dd40eaa7d7b0738a054f01be1a79826b1365
jtv <jtv@c3353b84-d008-0410-9ed9-b55c4f7fa7e8>
2015-01-23 18:18:41 +0000

    Modernize automake/autoconf setup.

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -8,17 +8,6 @@ set -e
 # bugs) involved in localization, encoding issues etc.  We only do ASCII here.
 #export LC_ALL=C
 
-latest_automake() {
-	for v in "-1.11" "-1.10" "-1.9" "-1.8" "-1.7" "-1.6" "" ; do
-		if which "automake$v" >/dev/null ; then
-			echo "$v"
-			return
-		fi
-	done
-}
-
-ver="`latest_automake`"
-
 # The VERSION file defines our versioning
 PQXXVERSION=`./tools/extract_version`
 echo "libpqxx version $PQXXVERSION"
@@ -63,8 +52,8 @@ fi
 autoheader
 
 libtoolize --force --automake --copy
-aclocal${ver} -I . -I config/m4
-automake${ver} --verbose --add-missing --copy
+aclocal -I . -I config/m4
+automake --verbose --add-missing --copy
 autoconf
 
 conf_flags="--enable-maintainer-mode $CONFIG_ARGS"
