7809d4aac178178f697528110c25743aba937a49
Jeroen Vermeulen <jtvjtv@gmail.com>
2017-06-19 23:09:56 +0200

    Generate ChangeLog from git log.

--- include/pqxx/version.hxx
+++ include/pqxx/version.hxx
@@ -18,15 +18,15 @@
 #ifndef PQXX_H_VERSION
 
 /// Full libpqxx version string.
-#define PQXX_VERSION "5.0.1"
+#define PQXX_VERSION "5.1.0"
 
 /// Major version number for this libpqxx release.
 #define PQXX_VERSION_MAJOR 5
 
 /// Minor version number for this libpqxx release.
-#define PQXX_VERSION_MINOR 0
+#define PQXX_VERSION_MINOR 1
 
 /// Library ABI version.
-#define PQXX_ABI "5.0"
+#define PQXX_ABI "5.1"
 
 #endif
