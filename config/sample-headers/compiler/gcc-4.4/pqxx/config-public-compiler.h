d4488f5e1e14dc553884bf3288cb95bbfd906c43
Jeroen Vermeulen <jtvjtv@gmail.com>
2017-07-23 14:44:52 +0200

    Drop obsolete sample compiler configs.

--- config/sample-headers/compiler/gcc-4.4/pqxx/config-public-compiler.h
+++
@@ -1,6 +0,0 @@
-/* Automatically generated from config.h: public/compiler config. */
-#define PQXX_HAVE_CPP_PRAGMA_MESSAGE 1
-#define PQXX_HAVE_GCC_CONST 1
-#define PQXX_HAVE_GCC_DEPRECATED 1
-#define PQXX_HAVE_GCC_NORETURN 1
-#define PQXX_HAVE_GCC_PURE 1
