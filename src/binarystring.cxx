dfe19c013a574f0836fb80f237eae67666f3d1b7
rocksolidwebdesign <vaughn@rocksolidwebdesign.com>
2017-10-22 14:56:41 -0600

    fixing using clauses

--- src/binarystring.cxx
+++ src/binarystring.cxx
@@ -54,7 +54,7 @@ buffer unescape(const unsigned char escaped[])
   size_t unescaped_len = 0;
   std::unique_ptr<unsigned char> A(
 	PQunescapeBytea(const_cast<unsigned char *>(escaped), &unescaped_len),
-	internal::freepqmem_templated<unsigned char>);
+	freepqmem_templated<unsigned char>);
   void *data = A.get();
   if (!data) throw std::bad_alloc();
   return to_buffer(data, unescaped_len);
