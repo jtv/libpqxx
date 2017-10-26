292741133fe20c97c1bce82198159dad810ab5ee
Jeroen Vermeulen <jtvjtv@gmail.com>
2017-10-26 22:40:32 +0200

    Extract documentation, reduce export surface.
    Removes documentation which is now in doc.hxx, un-exports some functions
    from the internal namespace which are never called from outside the
    library, and replaces some "extern const char[]" with constexpr.

--- include/pqxx/util.hxx
+++ include/pqxx/util.hxx
@@ -24,217 +24,6 @@
 #include "pqxx/strconv"
 
 
-/** @mainpage
- * @author Jeroen T. Vermeulen
- *
- * Welcome to libpqxx, the C++ API to the PostgreSQL database management system.
- *
- * Compiling this package requires PostgreSQL to be installed--including the C
- * headers for client development.  The library builds on top of PostgreSQL's
- * standard C API, libpq.  The libpq headers are not needed to compile client
- * programs, however.
- *
- * For a quick introduction to installing and using libpqxx, see the README.md
- * file; a more extensive tutorial is available in doc/html/Tutorial/index.html.
- *
- * The latest information can be found at http://pqxx.org/
- *
- * Some links that should help you find your bearings:
- * \li \ref gettingstarted
- * \li \ref threading
- * \li \ref connection
- * \li \ref transaction
- * \li \ref performance
- * \li \ref transactor
- *
- * @see http://pqxx.org/
- */
-
-/** @page gettingstarted Getting started
- * The most basic three types in libpqxx are the connection (which inherits its
- * API from pqxx::connection_base and its setup behaviour from
- * pqxx::connectionpolicy), the transaction (derived from
- * pqxx::transaction_base), and the result (pqxx::result).
- *
- * They fit together as follows:
- * \li You connect to the database by creating a
- * connection object (see \ref connection).  The connection type you'll usually
- * want is pqxx::connection.
- * \li You create a transaction object (see \ref transaction) operating on that
- * connection.  You'll usually want the pqxx::work variety.  If you don't want
- * transactional behaviour, use pqxx::nontransaction.  Once you're done you call
- * the transaction's @c commit function to make its work final.  If you don't
- * call this, the work will be rolled back when the transaction object is
- * destroyed.
- * \li Until then, use the transaction's @c exec function to execute queries,
- * which you pass in as simple strings.
- * \li The function returns a pqxx::result object, which acts as a standard
- * container of rows.  Each row in itself acts as a container of fields.  You
- * can use array indexing and/or iterators to access either.
- * \li The field's data is stored as a text string.  You can read it as such
- * using its @c c_str() function, or convert it to other types using its @c as()
- * and @c to() member functions.  These are templated on the destination type:
- * @c myfield.as<int>(); or @c myfield.to(myint);
- * \li After you've closed the transaction, the connection is free to run a next
- * transaction.
- *
- * Here's a very basic example.  It connects to the default database (you'll
- * need to have one set up), queries it for a very simple result, converts it to
- * an @c int, and prints it out.  It also contains some basic error handling.
- *
- * @code
- * #include <iostream>
- * #include <pqxx/pqxx>
- *
- * int main()
- * {
- *   try
- *   {
- *     pqxx::connection c;
- *     pqxx::work w(c);
- *     pqxx::result r = w.exec("SELECT 1");
- *     w.commit();
- *
- *     std::cout << r[0][0].as<int>() << std::endl;
- *   }
- *   catch (const std::exception &e)
- *   {
- *     std::cerr << e.what() << std::endl;
- *     return 1;
- *   }
- * }
- * @endcode
- *
- * This prints the number 1.  Notice that you can keep the result object
- * around after the transaction (or even the connection) has been closed.
- *
- * Here's a slightly more complicated example.  It takes an argument from the
- * command line and retrieves a string with that value.  The interesting part is
- * that it uses the escaping-and-quoting function @c quote() to embed this
- * string value in SQL safely.  It also reads the result field's value as a
- * plain C-style string using its @c c_str() function.
- *
- * @code
- * #include <iostream>
- * #include <pqxx/pqxx>
- *
- * int main(int argc, char *argv[])
- * {
- *   if (!argv[1])
- *   {
- *     std::cerr << "Give me a string!" << std::endl;
- *     return 1;
- *   }
- *   try
- *   {
- *     pqxx::connection c;
- *     pqxx::work w(c);
- *     pqxx::result r = w.exec("SELECT " + w.quote(argv[1]));
- *     w.commit();
- *
- *     std::cout << r[0][0].c_str() << std::endl;
- *   }
- *   catch (const std::exception &e)
- *   {
- *     std::cerr << e.what() << std::endl;
- *     return 1;
- *   }
- * }
- * @endcode
- *
- * You can find more about converting field values to native types, or
- * converting values to strings for use with libpqxx, under
- * \ref stringconversion.  More about getting to the rows and fields of a
- * result is under \ref accessingresults.
- *
- * If you want to handle exceptions thrown by libpqxx in more detail, for
- * example to print the SQL contents of a query that failed, see \ref exception.
- */
-
-/** @page accessingresults Accessing results and result rows
- *
- * Let's say you have a result object.  For example, your program may have done:
- *
- * @code
- * pqxx::result r = w.exec("SELECT * FROM mytable");
- * @endcode
- *
- * Now how do you access the data inside @c r?
- *
- * The simplest way is array indexing.  A result acts as an array of rows,
- * and a row acts as an array of fields.
- *
- * @code
- * const int num_rows = r.size();
- * for (int rownum=0; rownum < num_rows; ++rownum)
- * {
- *   const pqxx::row row = r[rownum];
- *   const int num_cols = row.size();
- *   for (int colnum=0; colnum < num_cols; ++colnum)
- *   {
- *     const pqxx::field field = row[colnum];
- *     std::cout << field.c_str() << '\t';
- *   }
- *
- *   std::cout << std::endl;
- * }
- * @endcode
- *
- * But results and rows also define @c const_iterator types:
- *
- * @code
- * for (const auto &row: r)
- *  {
- *    for (const auto &field: row) std::cout << field.c_str() << '\t';
- *    std::cout << std::endl;
- *  }
- * @endcode
- *
- * They also have @c const_reverse_iterator types, which iterate backwards from
- * @c rbegin() to @c rend() exclusive.
- *
- * All these iterator types provide one extra bit of convenience that you won't
- * normally find in C++ iterators: referential transparency.  You don't need to
- * dereference them to get to the row or field they refer to.  That is, instead
- * of @c row->end() you can also choose to say @c row.end().  Similarly, you
- * may prefer @c field.c_str() over @c field->c_str().
- *
- * This becomes really helpful with the array-indexing operator.  With regular
- * C++ iterators you would need ugly expressions like @c (*row)[0] or
- * @c row->operator[](0).  With the iterator types defined by the result and
- * row classes you can simply say @c row[0].
- */
-
-/** @page threading Thread safety
- *
- * This library does not contain any locking code to protect objects against
- * simultaneous modification in multi-threaded programs.  Therefore it is up
- * to you, the user of the library, to ensure that your threaded client
- * programs perform no conflicting operations concurrently.
- *
- * Most of the time this isn't hard.  Result sets are immutable, so you can
- * share them between threads without problem.  The main rule is:
- *
- * \li Treat a connection, together with any and all objects related to it, as
- * a "world" of its own.  You should generally make sure that the same "world"
- * is never accessed by another thread while you're doing anything non-const
- * in there.
- *
- * That means: don't issue a query on a transaction while you're also opening
- * a subtransaction, don't access a cursor while you may also be committing,
- * and so on.
- *
- * In particular, cursors are tricky.  It's easy to perform a non-const
- * operation without noticing.  So, if you're going to share cursors or
- * cursor-related objects between threads, lock very conservatively!
- *
- * Use @c pqxx::describe_thread_safety to find out at runtime what level of
- * thread safety is implemented in your build and version of libpqxx.  It
- * returns a pqxx::thread_safety_model describing what you can and cannot rely
- * on.  A command-line utility @c tools/pqxxthreadsafety prints out the same
- * information.
- */
-
 /// The home of all libpqxx classes, functions, templates, etc.
 namespace pqxx {}
 
@@ -284,12 +73,20 @@ struct PQXX_LIBEXPORT thread_safety_model
 /// Describe thread safety available in this build.
 PQXX_LIBEXPORT thread_safety_model describe_thread_safety() noexcept;
 
+
 /// The "null" oid.
 constexpr oid oid_none = 0;
 
 
-/// Access iterators using ACCESS functor, returning separator-separated list.
 /**
+ * @defgroup utility Utility functions
+ */
+//@{
+
+/// Represent sequence of values as a separated string.
+/**
+ * Use this to turn e.g. the numbers 1, 2, and 3 into a string "1, 2, 3".
+ *
  * @param sep separator string (to be placed between items)
  * @param begin beginning of items sequence
  * @param end end of items sequence
@@ -316,11 +113,6 @@ std::string separated_list(						//[t00]
 }
 
 
-/**
- * @defgroup utility Utility functions
- */
-//@{
-
 /// Render sequence as a string, using given separator between items.
 template<typename ITER> inline std::string
 separated_list(const std::string &sep, ITER begin, ITER end)		//[t00]
@@ -333,7 +125,8 @@ separated_list(const std::string &sep, const CONTAINER &c)		//[t10]
 	{ return separated_list(sep, c.begin(), c.end()); }
 //@}
 
-/// Private namespace for libpqxx's internal use; do not access
+
+/// Private namespace for libpqxx's internal use; do not access.
 /** This namespace hides definitions internal to libpqxx.  These are not
  * supposed to be used by client programs, and they may change at any time
  * without notice.
@@ -401,8 +194,10 @@ private:
 };
 
 
-void CheckUniqueRegistration(const namedclass *New, const namedclass *Old);
-void CheckUniqueUnregistration(const namedclass *New, const namedclass *Old);
+PQXX_PRIVATE void CheckUniqueRegistration(
+        const namedclass *New, const namedclass *Old);
+PQXX_PRIVATE void CheckUniqueUnregistration(
+        const namedclass *New, const namedclass *Old);
 
 
 /// Ensure proper opening/closing of GUEST objects related to a "host" object
@@ -447,7 +242,10 @@ using cstring = const char *;
 
 
 /// Commonly used SQL commands
-extern const char sql_begin_work[], sql_commit_work[], sql_rollback_work[];
+constexpr char
+        sql_begin_work[] = "BEGIN",
+        sql_commit_work[] = "COMMIT",
+        sql_rollback_work[] = "ROLLBACK";
 
 } // namespace internal
 } // namespace pqxx
