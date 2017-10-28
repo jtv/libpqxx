7ab4e7357d5a5743494a8ecbf40f0ea5dc1e625d
Jeroen Vermeulen <jtvjtv@gmail.com>
2017-10-28 22:25:34 +0200

    Add sample headers for gcc 7.2.

---
+++ config/sample-headers/compiler/gcc-7.2/pqxx/config-internal-compiler.h
@@ -0,0 +1,5 @@
+/* Automatically generated from config.h: internal/compiler config. */
+
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define PQXX_HAVE_GCC_VISIBILITY 1
---
+++ config/sample-headers/compiler/gcc-7.2/pqxx/config-public-compiler.h
@@ -0,0 +1,6 @@
+/* Automatically generated from config.h: public/compiler config. */
+
+#define PQXX_HAVE_DEPRECATED 1
+#define PQXX_HAVE_EXP_OPTIONAL 1
+#define PQXX_HAVE_GCC_CONST 1
+#define PQXX_HAVE_GCC_PURE 1
