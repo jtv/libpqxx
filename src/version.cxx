c53857bb2db45e2d0073cc91d955c4eaf69d6d5b
Jeroen Vermeulen <jtvjtv@gmail.com>
2018-05-04 22:16:10 +0200

    Fix Windows DLL export of version-check function.
    Apparently this is needed to get rid of a shared-library link error with
    Visual Studio: add PQXX_LIBEXPORT to the function's specialisation —
    in the source file, not just the header.  Thanks edwinvp.
    
    Fixes #86

--- src/version.cxx
+++ src/version.cxx
@@ -9,7 +9,7 @@ namespace internal
 // One, single definition of this function.  If a call fails to link, then the
 // libpqxx binary was built against a different libpqxx version than the code
 // which is being linked against it.
-template<>
+template<> PQXX_LIBEXPORT
 int check_library_version<PQXX_VERSION_MAJOR, PQXX_VERSION_MINOR>() noexcept
 {
   return 0;
