79003942780d0b6345e90961a2ac62ce035673ee
Jeroen Vermeulen <jtvjtv@gmail.com>
2019-02-08 20:45:16 +0100

    Fix `auto` initialiser.
    Fixes #161.

--- src/encodings.cxx
+++ src/encodings.cxx
@@ -687,7 +687,7 @@ encoding_group enc_group(const std::string& encoding_name)
     {"WIN1258", encoding_group::MONOBYTE},
   };
   
-  auto found_encoding_group{encoding_map.find(encoding_name)};
+  const auto found_encoding_group = encoding_map.find(encoding_name);
   if (found_encoding_group == encoding_map.end())
     throw std::invalid_argument{
       "unrecognized encoding '" + encoding_name + "'"
