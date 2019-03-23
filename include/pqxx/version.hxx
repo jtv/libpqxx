be6fe11a8e1eeaf12b73e776ba3998d9cfb36dcf
Jeroen Vermeulen <jtvjtv@gmail.com>
2019-03-23 01:21:41 +0100

    Bump to 6.4.3.

--- include/pqxx/version.hxx
+++ include/pqxx/version.hxx
@@ -14,7 +14,7 @@
 #include "pqxx/compiler-internal-pre.hxx"
 
 /// Full libpqxx version string.
-#define PQXX_VERSION "6.4.2"
+#define PQXX_VERSION "6.4.3"
 /// Library ABI version.
 #define PQXX_ABI "6.4"
 
