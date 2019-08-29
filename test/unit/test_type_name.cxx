6c1e88fa5dcbae6529a39dee3bb3c17f470bbab4
Jeroen Vermeulen <jtvjtv@gmail.com>
2019-08-29 14:52:02 +0200

    Don't test `type_name` for class types.
    Visual Studio prefixes class type names with `class`.  :-(
    
    Fixes #212.

--- test/unit/test_type_name.cxx
+++ test/unit/test_type_name.cxx
@@ -6,15 +6,12 @@ void test_type_name()
 {
   // It's hard to test in more detail, because spellings may differ.
   // For instance, one compiler might call "const unsigned int" what another
-  // might call "unsigned const".
+  // might call "unsigned const".  And Visual Studio prefixes "class" to class
+  // types.
   PQXX_CHECK_EQUAL(
 	pqxx::type_name<int>,
 	"int",
 	"type_name<int> came out weird.");
-  PQXX_CHECK_EQUAL(
-	pqxx::type_name<std::shared_ptr<int>>,
-	"std::shared_ptr<int>",
-	"type_name<std::shared_ptr<int>> came out weird.");
 }
 
 
