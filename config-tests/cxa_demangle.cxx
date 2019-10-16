f6f3515e5ae6d26a2aceb51ded25eb4b633983e6
Jeroen Vermeulen <jtvjtv@gmail.com>
2019-10-16 12:24:26 +0200

    Cosmetic.

--- config-tests/cxa_demangle.cxx
+++ config-tests/cxa_demangle.cxx
@@ -1,15 +1,16 @@
 // Test for cross-vendor C++ ABI's __cxa_demangle function.
-#include <cxxabi.h>
 #include <cstdlib>
 #include <cstring>
 #include <stdexcept>
 #include <typeinfo>
 
+#include <cxxabi.h>
+
 int main()
 {
   int status = 0;
-  char *name = abi::__cxa_demangle(
-	typeid(10).name(), nullptr, nullptr, &status);
+  char *name =
+    abi::__cxa_demangle(typeid(10).name(), nullptr, nullptr, &status);
   if (status != 0)
     throw std::runtime_error("Demangle failed!");
   int result = std::strcmp(name, "int");
