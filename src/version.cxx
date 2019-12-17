039c69654a5f231b9135cee303b394691765b641
Jeroen Vermeulen <jtvjtv@gmail.com>
2019-12-17 16:31:53 +0100

    Export version specialisation.

--- src/version.cxx
+++ src/version.cxx
@@ -15,15 +15,11 @@ namespace pqxx::internal
 // One, single definition of this function.  If a call fails to link, then the
 // libpqxx binary was built against a different libpqxx version than the code
 // which is being linked against it.
-template<>
-int check_library_version<PQXX_VERSION_MAJOR, PQXX_VERSION_MINOR>() noexcept
-{
-  return 0;
-}
+template<int, int> PQXX_LIBEXPORT int check_library_version() noexcept
+{ return 0; }
 
 
-// Now explicitly instantiate the explicit specialisation, just to get
-// Visual Studio to export it when we're building a DLL.
-template
-int check_library_version<PQXX_VERSION_MAJOR, PQXX_VERSION_MINOR>() noexcept;
+// Export an explicit instantiation.
+template PQXX_LIBEXPORT int
+check_library_version<PQXX_VERSION_MAJOR, PQXX_VERSION_MINOR>() noexcept;
 } // namespace pqxx::internal
