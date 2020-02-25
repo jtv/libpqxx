16764b690adc0f1411455e325e6a4883ff8340fc
Jeroen Vermeulen <jtvjtv@gmail.com>
2020-02-25 20:27:19 +0100

    Test for nasty largeobject stream bug.
    @tomlankhorst has discovered a nasty bug with `ilostream`: on systems
    where `char` is signed, if a large object contains a byte value `0xff`
    (i.e. -1 as a signed char) at a buffer boundary, that byte gets
    mistaken for a -1, i.e. an end-of-file marker.
    
    See #284 for bug details, and #286 for Tom's fix.  We'll briefly have
    failing tests before I merge his fix.

---
+++ test/unit/test_largeobject.cxx
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sstream>
+
+#include "test_helpers.hxx"
+
+
+namespace
+{
+void test_stream_large_object()
+{
+  pqxx::connection conn;
+
+  // Construct a really nasty string.  (Don't just construct a std::string from
+  // a char[] constant, because it'll terminate at the embedded zero.)
+  //
+  // The crucial thing is the "ff" byte at the beginning.  It tests for
+  // possible conflation between "eof" (-1) and a char which just happens to
+  // have the same bit pattern as an 8-bit value of -1.  This conflation can be
+  // a problem when it occurs at buffer boundaries.
+  char const bytes[]{"\xff\0end"};
+  std::string const contents{bytes, sizeof(bytes)};
+
+  pqxx::work tx{conn};
+  pqxx::largeobject new_obj{tx};
+
+  pqxx::olostream write{tx, new_obj};
+  write << contents;
+  write.flush();
+
+  pqxx::largeobjectaccess check{tx, new_obj, std::ios::in | std::ios::binary};
+  std::array<char, 50> buf;
+  std::size_t const len{static_cast<std::size_t>(check.read(buf.data(), buf.size()))};
+  PQXX_CHECK_EQUAL(len, contents.size(), "olostream truncated data.");
+  std::string const check_str{buf.data(), len};
+  PQXX_CHECK_EQUAL(check_str, contents, "olostream mangled data.");
+
+  pqxx::ilostream read{tx, new_obj};
+  std::string read_back;
+  std::string chunk;
+  while (read >> chunk) read_back += chunk;
+
+  new_obj.remove(tx);
+
+  PQXX_CHECK_EQUAL(
+    read_back, contents, "Got wrong data from ilostream.");
+  PQXX_CHECK_EQUAL(
+    read_back.size(), contents.size(), "ilostream truncated data.");
+  PQXX_CHECK_EQUAL(
+    read_back.size(), sizeof(bytes), "ilostream truncated data.");
+}
+
+
+PQXX_REGISTER_TEST(test_stream_large_object);
+} // namespace
