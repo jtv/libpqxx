62dabdfce971303ef597fe2858a1394fcd729848
Jeroen Vermeulen <jtvjtv@gmail.com>
2020-03-23 23:58:13 +0100

    Prefer `pg_config` over `pkg-config` for headers.
    Works around #291.  In the long run though, we'll want to go with
    `pkg-config` since `pg_config` is deprecated.
    
    Also, the configure script will no longer fail in the case where neither
    `pkg-config` nor `pg_config` is available, and the
    `--with-postres-include` option was not specified, but the compiler can
    find the postgres headers anyway.
    
    Bumped VERSION to 7.0.6.

--- include/pqxx/doc/mainpage.md
+++ include/pqxx/doc/mainpage.md
@@ -1,7 +1,7 @@
 libpqxx                                      {#mainpage}
 =======
 
-@version 7.0.5
+@version 7.0.6
 @author Jeroen T. Vermeulen
 @see http://pqxx.org
 @see https://github.com/jtv/libpqxx
--- include/pqxx/version.hxx
+++ include/pqxx/version.hxx
@@ -14,7 +14,7 @@
 #  include "pqxx/internal/compiler-internal-pre.hxx"
 
 /// Full libpqxx version string.
-#  define PQXX_VERSION "7.0.5"
+#  define PQXX_VERSION "7.0.6"
 /// Library ABI version.
 #  define PQXX_ABI "7.0"
 
