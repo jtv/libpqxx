275d0f24f4e1d6983729bf24d8d1d99ab4686c03
Jeroen Vermeulen <jtvjtv@gmail.com>
2020-04-19 15:25:06 +0200

    Move test93 to `test/unit/`.
    Chipping away at the old, numbered, story tests because I much prefer
    the newer unit tests.  In the end I want to be able to run tests in
    parallel.  And I want to eliminate redundancy among story tests, many of
    which I copied from older ones and which have since rusted in place
    because they became part of "story lines" across multiple test files.
    This test isn't one of those cases, but right now there's just too many
    of these tests to get good insight.
    
    Building tests takes up most of the build time.  Have to cut into that
    time to get major improvements.  I may also narrow down the include
    trees a bit, just to reduce the amount of pointless work the
    preprocessor and compiler do for each test.

--- test/unit/test_column.cxx
+++ test/unit/test_column.cxx
@@ -1,27 +1,16 @@
-#include <iostream>
-#include <set>
-#include <vector>
+#include "../test_helpers.hxx"
 
-#include "test_helpers.hxx"
-
-
-// Test program for libpqxx.  Test querying of result column origins.
 namespace
 {
-void test_093()
+void test_table_column()
 {
-  pqxx::result R, X;
+  pqxx::connection conn;
+  pqxx::work tx{conn};
 
-  {
-    pqxx::connection conn;
-    pqxx::work tx{conn};
-    tx.exec0("CREATE TEMP TABLE pqxxfoo (x varchar, y integer, z integer)");
-    tx.exec0("INSERT INTO pqxxfoo VALUES ('xx', 1, 2)");
-    R = tx.exec("SELECT z,y,x FROM pqxxfoo");
-    X = tx.exec("SELECT x,y,z,99 FROM pqxxfoo");
-
-    // tx and connection are closed here; result objects remain
-  }
+  tx.exec0("CREATE TEMP TABLE pqxxfoo (x varchar, y integer, z integer)");
+  tx.exec0("INSERT INTO pqxxfoo VALUES ('xx', 1, 2)");
+  auto R{tx.exec("SELECT z,y,x FROM pqxxfoo")};
+  auto X{tx.exec("SELECT x,y,z,99 FROM pqxxfoo")};
 
   pqxx::row::size_type x{R.table_column(2)}, y{R.table_column(1)},
     z{R.table_column(static_cast<int>(0))};
@@ -64,7 +53,9 @@ void test_093()
     col = X.table_column(3), "table_column() on non-table didn't fail.");
   pqxx::ignore_unused(col);
 }
+
+// TODO: Test interaction with result slicing.
 } // namespace
 
 
-PQXX_REGISTER_TEST(test_093);
+PQXX_REGISTER_TEST(test_table_column);
