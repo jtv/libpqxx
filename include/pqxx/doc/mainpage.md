365b31fee637da4d99ecfd237b64f4e6d9f0691c
Jeroen T. Vermeulen <jtvjtv@gmail.com>
2020-06-28 01:17:57 +0200

    Bump to 7.1.3.
    I may end up going straight to 7.2.0 though, to make some more
    far-reaching changes:
     * Use proper `time_t` in `wait_read` etc. (#339)
     * Move exception destructors into `except.cxx` for slimmer binaries.
     * Dip a toe into C++20 and concepts for `PQconnectdbParams()`.  (#343)

--- include/pqxx/doc/mainpage.md
+++ include/pqxx/doc/mainpage.md
@@ -1,7 +1,7 @@
 libpqxx                                      {#mainpage}
 =======
 
-@version 7.1.2
+@version 7.1.3
 @author Jeroen T. Vermeulen
 @see http://pqxx.org
 @see https://github.com/jtv/libpqxx
--- include/pqxx/version.hxx
+++ include/pqxx/version.hxx
@@ -14,7 +14,7 @@
 #  include "pqxx/internal/compiler-internal-pre.hxx"
 
 /// Full libpqxx version string.
-#  define PQXX_VERSION "7.1.2"
+#  define PQXX_VERSION "7.1.3"
 /// Library ABI version.
 #  define PQXX_ABI "7.1"
 
