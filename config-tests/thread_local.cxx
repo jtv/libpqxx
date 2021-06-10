b914fb3dca7e77e0d4be2758f73b42f11ad6c4a6
Jeroen Vermeulen <jtvjtv@gmail.com>
2021-06-10 14:04:45 +0200

    For #453: MinGW's gcc 11.0 is also broken.

--- config-tests/thread_local.cxx
+++ config-tests/thread_local.cxx
@@ -4,8 +4,10 @@
 
 int main(int argc, char **)
 {
-#if defined(__MINGW32__) && defined(__GNUC__) && __GNUC__ < 11
-#  error "This MinGW gcc's thread_local breaks at run time, sorry."
+#if defined(__MINGW32__) && defined(__GNUC__)
+#  if __GNUC__ < 11 || ((__GNUC__ == 11) && (__GNU_MINOR__ == 0))
+#    error "On MinGW before gcc 11.1, thread_local breaks at run time."
+#  endif
 #endif
   thread_local std::stringstream s;
   s << argc;
