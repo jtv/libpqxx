f1f7f7cbefade3a7df94a49e5f949af5c6da6751
Jeroen T. Vermeulen <jtvjtv@gmail.com>
2021-12-15 09:02:38 +0100

    Prepare for 6.4.9.

--- include/pqxx/version.hxx
+++ include/pqxx/version.hxx
@@ -14,7 +14,7 @@
 #include "pqxx/compiler-internal-pre.hxx"
 
 /// Full libpqxx version string.
-#define PQXX_VERSION "6.4.8"
+#define PQXX_VERSION "6.4.9"
 /// Library ABI version.
 #define PQXX_ABI "6.4"
 
