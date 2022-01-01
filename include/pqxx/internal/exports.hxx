d95fac1d4fb23addd4fdd945e782461c4e426a57
Jeroen T. Vermeulen <jtvjtv@gmail.com>
2022-01-01 19:15:16 +0100

    Do DLL std exports in a header.

---
+++ include/pqxx/internal/exports.hxx
@@ -0,0 +1,37 @@
+/** Standard library symbols that we need to export for Windows DLL build.
+ *
+ * Apparently DLLs on Windows can't just throw or return standard library types
+ * such as @c std::string or @c std::runtime_error.  The library needs to
+ * export those explicitly.
+ */
+#if !defined(PQXX_H_EXPORTS) && defined(_WIN32) && defined(PQXX_SHARED)
+#  define PQXX_H_EXPORTS
+
+#  include <cstddef>
+#  include <memory>
+#  include <stdexcept>
+#  include <string>
+#  include <string_view>
+#  include <vector>
+
+#  include "pqxx/types.hxx"
+
+
+template class PQXX_LIBEXPORT
+  std::basic_string<char, std::char_traits<char>, std::allocator<char>>;
+template class PQXX_LIBEXPORT std::basic_string_view<char>;
+class PQXX_LIBEXPORT std::domain_error;
+class PQXX_LIBEXPORT std::invalid_argument;
+class PQXX_LIBEXPORT std::logic_error;
+class PQXX_LIBEXPORT std::out_of_range;
+class PQXX_LIBEXPORT std::runtime_error;
+template class PQXX_LIBEXPORT std::shared_ptr<std::string>;
+template class PQXX_LIBEXPORT std::vector < pqxx::format,
+  std::allocator<pqxx::format>;
+template class PQXX_LIBEXPORT std::vector < char const *,
+  std::allocator<char const *>;
+template class PQXX_LIBEXPORT std::vector < int, std::allocator<int>;
+template class PQXX_LIBEXPORT std::vector < std::string_view,
+  std::allocator<std::string_view>;
+
+#endif
