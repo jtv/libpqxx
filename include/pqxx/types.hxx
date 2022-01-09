9a4742823c5f5cb805c086e0bea6580a981603c2
Jeroen T. Vermeulen <jtvjtv@gmail.com>
2022-01-09 17:43:15 +0100

    Update copyright strings.

--- include/pqxx/types.hxx
+++ include/pqxx/types.hxx
@@ -1,6 +1,6 @@
 /* Basic type aliases and forward declarations.
  *
- * Copyright (c) 2000-2021, Jeroen T. Vermeulen
+ * Copyright (c) 2000-2022, Jeroen T. Vermeulen
  *
  * See COPYING for copyright license.  If you did not receive a file called
  * COPYING with this source code, please notify the distributor of this
