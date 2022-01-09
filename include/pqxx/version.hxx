74dc9f68e703ed5d151bcd582d50594959e6e5bd
Jeroen T. Vermeulen <jtvjtv@gmail.com>
2022-01-09 17:48:17 +0100

    Bump version.

--- include/pqxx/version.hxx
+++ include/pqxx/version.hxx
@@ -14,7 +14,7 @@
 #  include "pqxx/internal/compiler-internal-pre.hxx"
 
 /// Full libpqxx version string.
-#  define PQXX_VERSION "7.5.3"
+#  define PQXX_VERSION "7.5.4"
 /// Library ABI version.
 #  define PQXX_ABI "7.5"
 
