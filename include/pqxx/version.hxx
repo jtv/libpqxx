7f3be09be1ab89351e7d7767de9a6bc6b1947761
Jeroen T. Vermeulen <jtvjtv@gmail.com>
2022-01-09 17:44:37 +0100

    Forgot to bump version.

--- include/pqxx/version.hxx
+++ include/pqxx/version.hxx
@@ -14,7 +14,7 @@
 #  include "pqxx/internal/compiler-internal-pre.hxx"
 
 /// Full libpqxx version string.
-#  define PQXX_VERSION "7.5.2"
+#  define PQXX_VERSION "7.5.3"
 /// Library ABI version.
 #  define PQXX_ABI "7.5"
 
