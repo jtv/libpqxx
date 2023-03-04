f65a636ef867af9b67dcde456af89daedc75e2cc
Jeroen Vermeulen <jtvjtv@gmail.com>
2023-03-04 14:22:07 +0100

    Typo.

--- config-tests/decimal-fp.cxx
+++ config-tests/decimal-fp.cxx
@@ -1,4 +1,4 @@
 // Test for C23 decimal floating-point types.
-_Decimal32 d32 const = 3.2;
-_Decimal64 d64 const = 6.4;
-_Decimal128 d128 const = 1.28;
+_Decimal32 const d32 = 3.2;
+_Decimal64 const d64 = 6.4;
+_Decimal128 const d128 = 1.28;
