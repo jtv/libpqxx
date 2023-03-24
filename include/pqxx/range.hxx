8857ff3c9361560d6ddef512032964ad7c9c61b2
Jeroen Vermeulen <jtvjtv@gmail.com>
2023-03-24 20:41:18 +0100

    Document build workarounds contributed by @skasperski.

--- include/pqxx/range.hxx
+++ include/pqxx/range.hxx
@@ -41,6 +41,7 @@ struct no_bound
  */
 template<typename TYPE> class inclusive_bound
 {
+// (Putting private section first to work around bug in gcc < 10: see #665.)
 private:
   TYPE m_value;
 
@@ -76,6 +77,7 @@ public:
  */
 template<typename TYPE> class exclusive_bound
 {
+// (Putting private section first to work around bug in gcc < 10: see #665.)
 private:
   TYPE m_value;
 
@@ -111,6 +113,7 @@ public:
  */
 template<typename TYPE> class range_bound
 {
+// (Putting private section first to work around bug in gcc < 10: see #665.)
 private:
   std::variant<no_bound, inclusive_bound<TYPE>, exclusive_bound<TYPE>> m_bound;
 
@@ -229,6 +232,7 @@ public:
  */
 template<typename TYPE> class range
 {
+// (Putting private section first to work around bug in gcc < 10: see #665.)
 private:
   range_bound<TYPE> m_lower, m_upper;
 
