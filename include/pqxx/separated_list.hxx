7234da0e0202dd7b53b3f051574e6cdd8b3e6743
Jeroen Vermeulen <jtvjtv@gmail.com>
2024-03-11 13:49:02 +0100

    Note.

--- include/pqxx/separated_list.hxx
+++ include/pqxx/separated_list.hxx
@@ -22,6 +22,7 @@
 
 // C++20: Simplify using std::ranges::range.
 // C++20: Optimise buffer allocation using random_access_range/iterator.
+// C++23: Use std::join_with().
 // TODO: Can we pass separators at compile time?
 namespace pqxx
 {
