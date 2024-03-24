1465d95152ac87fdd3a5d089cd68cfeb0223578b
Jeroen Vermeulen <jtvjtv@gmail.com>
2024-03-24 22:23:03 +0100

    Sphinx tweaks.

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,6 @@ if read_the_docs_build:
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
     ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,8 +49,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
