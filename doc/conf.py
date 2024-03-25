b7cdeb9327cf8ac817355cd2ae76b063e9bdde02
Jeroen Vermeulen <jtvjtv@gmail.com>
2024-03-25 19:34:30 +0100

    Sphinx does not tolerate Path.

--- doc/conf.py
+++ doc/conf.py
@@ -132,7 +132,7 @@ html_sidebars = {
 # has readthedocs copy the full generated HTML tree to the output directory.
 #
 # Problem is, that doesn't seem to be working now.  This needs debugging.
-html_extra_path = [Path(__file__).parent.absolute() / "html"]
+html_extra_path = [str(Path(__file__).parent.absolute() / "html")]
 
 # -- Options for HTMLHelp output ------------------------------------------
 
