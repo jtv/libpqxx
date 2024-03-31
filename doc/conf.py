7b71e7fe042e70df3429ff615f88ae3045711745
Jeroen Vermeulen <jtvjtv@gmail.com>
2024-03-31 01:24:31 +0100

    Don't use sphinx autodoc extension.

--- doc/conf.py
+++ doc/conf.py
@@ -40,9 +40,9 @@ if os.environ.get('READTHEDOCS', '').strip() == 'True':
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    ]
+#extensions = [
+#    'sphinx.ext.autodoc',
+#    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
