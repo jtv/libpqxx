aa596a2263225ab9b258aeebc66b8d20d9fc7149
Jeroen Vermeulen <jtvjtv@gmail.com>
2024-05-15 00:27:36 +0200

    Try _not_ calling doxygen ourselves.

--- doc/conf.py
+++ doc/conf.py
@@ -70,7 +70,7 @@ if os.environ.get('READTHEDOCS') == 'True':
     # C++23: Upgrade C++ version.
     check_call(
         [source_dir / 'configure', 'CXXFLAGS=-std=c++20 -O0'], cwd=build_dir)
-    check_call('doxygen', cwd=(build_dir / 'doc'))
+#    check_call('doxygen', cwd=(build_dir / 'doc'))
 
 
 # -- General configuration ------------------------------------------------
