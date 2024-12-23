e98e74cff38d818259cb5917126086f705582b1d
Jeroen Vermeulen <jtvjtv@gmail.com>
2024-12-23 15:33:39 +0100

    Retire `binarystring`.
    This type has been deprecated since 7.2.0, more than 4 years ago.

--- test/unit/test_prepared_statement.cxx
+++ test/unit/test_prepared_statement.cxx
@@ -177,16 +177,6 @@ void test_binary()
   constexpr char raw_bytes[]{"Binary\0bytes'\"with\tweird\xff bytes"};
   std::string const input{raw_bytes, std::size(raw_bytes)};
 
-#include "pqxx/internal/ignore-deprecated-pre.hxx"
-  {
-    pqxx::binarystring const bin{input};
-    auto rw{tx.exec(pqxx::prepped{"EchoBin"}, pqxx::params{bin}).one_row()};
-    PQXX_CHECK_EQUAL(
-      pqxx::binarystring(rw[0]).str(), input,
-      "Binary string came out damaged.");
-  }
-#include "pqxx/internal/ignore-deprecated-post.hxx"
-
   {
     pqxx::bytes bytes{
       reinterpret_cast<std::byte const *>(raw_bytes), std::size(raw_bytes)};
