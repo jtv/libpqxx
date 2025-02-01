1b065a5c6cbfafec83647587ae9e65bb03d063a4
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-02-01 23:56:02 +0100

    Fix.

--- src/result.cxx
+++ src/result.cxx
@@ -318,7 +318,7 @@ void pqxx::result::check_status(std::string_view desc, PQXX_LOC loc) const
 std::string pqxx::result::status_error(PQXX_LOC loc) const
 {
   if (m_data.get() == nullptr)
-    throw failure{"No result set given."};
+    throw failure{"No result set given.", loc};
 
   std::string err;
 
@@ -335,7 +335,7 @@ std::string pqxx::result::status_error(PQXX_LOC loc) const
 #if defined(LIBPQ_HAS_PIPELINING)
   case PGRES_PIPELINE_SYNC:    // Pipeline mode synchronisation point.
   case PGRES_PIPELINE_ABORTED: // Previous command in pipeline failed.
-    throw feature_not_supported{"Not supported yet: libpq pipelines."};
+    throw feature_not_supported{"Not supported yet: libpq pipelines.", loc};
 #endif
 
   case PGRES_BAD_RESPONSE: // The server's response was not understood.
@@ -345,12 +345,12 @@ std::string pqxx::result::status_error(PQXX_LOC loc) const
     break;
 
   case PGRES_SINGLE_TUPLE:
-    throw feature_not_supported{"Not supported: single-row mode."};
+    throw feature_not_supported{"Not supported: single-row mode.", loc};
 
   default:
     throw internal_error{internal::concat(
       "pqxx::result: Unrecognized result status code ",
-      PQresultStatus(m_data.get()))};
+      PQresultStatus(m_data.get())), loc};
   }
   return err;
 }
