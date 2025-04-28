e56175d16064e389631cd445d6c74b93ed72edec
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-04-28 19:15:15 +0200

    992: Support spaces in connection string params.
    These are just the tests.  I'll write the code next (and probably find
    mistakes in the test, but such is life).
    
    This is still minimal...  What of weird multibyte encodings?  What of
    inconsistently URI-encoded quotes or backslashes?  I hope this is not a
    huge rabbit hole.

---
+++ test/test_connection.cxx
@@ -0,0 +1,64 @@
+#include <format>
+
+#include <pqxx/pqxx>
+
+#include "test_helpers.hxx"
+
+
+namespace
+{
+constexpr std::string_view const marker{"application_name="};
+
+
+/// Connect to the database, passing `app_name` for its `application_name`.
+/** Does not do any quoting or escaping.  The caller will have to do that.
+ */
+pqxx::connection connect(std::string const &app_name)
+{
+  return pqxx::connection{std::format("{}{}", marker, app_name)};
+}
+
+
+/// Extract the application name from a connection's connection string.
+/** Does not do any un-escaping, does not remove quotes.  This is deliberate.
+ *
+ * Also does not handle much in the way of weird inputs.  It just looks for
+ * the first instance of "application_name=" and goes from there.  This is
+ * because I'm lazy and I see no substantial security risks in this.
+ */
+std::string app_name(pqxx::connection const &cx)
+{
+  std::string const &all{cx.connection_string()};
+
+  auto const intro{all.find(marker)};
+  auto const start{intro + std::size(marker)};
+  if (start == std::string::npos or all.at(start) == ' ') return "";
+  auto here{start};
+  if (all.at(here) == '\'')
+  {
+    // Quoted string.  Be prepared for escaping.
+    ++here;
+    for (bool esc{false}; (here < std::size(all)) and ((all.at(here) != '\'') or esc); ++here) esc = (all.at(here) == '\\' and not esc);
+  }
+  else
+  {
+    // Simple string.  Just stop at the next space, or end of string.
+    // This find() can return npos, but that's fine here.
+    here = all.find(' ', start);
+  }
+    return all.substr(start, all.find(' ', start) - start);
+}
+
+
+void test_connection_string_escapes()
+{
+  PQXX_CHECK_EQUAL(app_name(connect("pqxxtest")), "pqxxtest", "Simple connection param came back wrong.");
+  PQXX_CHECK_EQUAL(app_name(connect("'hello'")), "hello", "Quoted connection param came back wrong.");
+  PQXX_CHECK_EQUAL(app_name(connect("'a b c'")), "'a b c'", "Spaces in param went bad.");
+  PQXX_CHECK_EQUAL(app_name(connect("'x\\\\y'")), "'x\\y'", "Backslash escaping failed.");
+  PQXX_CHECK_EQUAL(app_name(connect("'don\\'t'")), "'don\\'t'", "Unexpected backslash escaping.");
+}
+
+
+PQXX_REGISTER_TEST(test_connection_string_escapes);
+} // namespace
