3d8a4d6c1fc14b39a703ad1b88309197a1fabaed
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-06-19 21:06:54 +0200

    Work around MSVC missing support for `__VA_OPT__`.
    For some reason I can't get the Visual Studio build to accept this
    built-in preprocessor symbol, introduced in C++20.  I tried passing
    `/std:c++20` and `/Zc:preprocessor` but they made no difference.
    
    The weird thing is, this is Visual Studio 2022.  Support was
    supposedly added somewhere in Visual Studio 2019.

--- test/helpers.hxx
+++ test/helpers.hxx
@@ -58,17 +58,32 @@ struct registrar
 
 // Verify that a condition is met, similar to assert().
 // Takes an optional failure description as a second argument.
-#define PQXX_CHECK(condition, ...)                                            \
-  pqxx::test::check((condition), #condition __VA_OPT__(, ) __VA_ARGS__)
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+// Work around Visual Studio lacking support for __VA_OPT__.
+#  define PQXX_CHECK(condition, ...)                                          \
+    pqxx::test::check((condition), #condition, ##__VA_ARGS__)
+#else
+#  define PQXX_CHECK(condition, ...)                                          \
+    pqxx::test::check((condition), #condition __VA_OPT__(, ) __VA_ARGS__)
+#endif
+
 void check(
   bool condition, char const text[],
   std::string const &desc = "Condition check failed,", sl loc = sl::current());
 
 // Verify that variable has the expected value.
 // Takes an optional failure description as a third argument.
-#define PQXX_CHECK_EQUAL(actual, expected, ...)                               \
-  pqxx::test::check_equal(                                                    \
-    (actual), #actual, (expected), #expected __VA_OPT__(, ) __VA_ARGS__)
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+// Work around Visual Studio lacking support for __VA_OPT__.
+#  define PQXX_CHECK_EQUAL(actual, expected, ...)                             \
+    pqxx::test::check_equal(                                                  \
+      (actual), #actual, (expected), #expected, ##__VA_ARGS__)
+#else
+#  define PQXX_CHECK_EQUAL(actual, expected, ...)                             \
+    pqxx::test::check_equal(                                                  \
+      (actual), #actual, (expected), #expected __VA_OPT__(, ) __VA_ARGS__)
+#endif
+
 template<typename ACTUAL, typename EXPECTED>
 inline void check_equal(
   ACTUAL const &actual, char const actual_text[], EXPECTED const &expected,
@@ -90,9 +105,16 @@ inline void check_equal(
 
 // Verify that two values are not equal.
 // Takes an optional failure description as a third argument.
-#define PQXX_CHECK_NOT_EQUAL(value1, value2, ...)                             \
-  pqxx::test::check_not_equal(                                                \
-    (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+// Work around Visual Studio lacking support for __VA_OPT__.
+#  define PQXX_CHECK_NOT_EQUAL(value1, value2, ...)                           \
+    pqxx::test::check_not_equal(                                              \
+      (value1), #value1, (value2), #value2, ##__VA_ARGS__)
+#else
+#  define PQXX_CHECK_NOT_EQUAL(value1, value2, ...)                           \
+    pqxx::test::check_not_equal(                                              \
+      (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
+#endif
 template<typename VALUE1, typename VALUE2>
 inline void check_not_equal(
   VALUE1 const &value1, char const text1[], VALUE2 const &value2,
@@ -109,16 +131,20 @@ inline void check_not_equal(
 }
 
 
-// Verify that value1 is less than value2.
+// Verify that value1 is less/greater than value2.
 // Takes an optional failure description as a third argument.
-#define PQXX_CHECK_LESS(value1, value2, ...)                                  \
-  pqxx::test::check_less(                                                     \
-    (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
-// Verify that value1 is greater than value2.
-// Takes an optional failure description as a third argument.
-#define PQXX_CHECK_GREATER(value2, value1, ...)                               \
-  pqxx::test::check_less(                                                     \
-    (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+// Work around Visual Studio lacking support for __VA_OPT__.
+#  define PQXX_CHECK_LESS(value1, value2, ...)                                \
+    pqxx::test::check_less((value1), #value1, (value2), #value2, ##__VA_ARGS__)
+#  define PQXX_CHECK_GREATER(value2, value1, ...)                             \
+    pqxx::test::check_less((value1), #value1, (value2), #value2, ##__VA_ARGS__)
+#else
+#  define PQXX_CHECK_LESS(value1, value2, ...)                                \
+    pqxx::test::check_less((value1), #value1, (value2), #value2, ##__VA_ARGS__)
+#  define PQXX_CHECK_GREATER(value2, value1, ...)                             \
+    pqxx::test::check_less((value1), #value1, (value2), #value2, ##__VA_ARGS__)
+#endif
 template<typename VALUE1, typename VALUE2>
 inline void check_less(
   VALUE1 const &value1, char const text1[], VALUE2 const &value2,
@@ -138,16 +164,24 @@ inline void check_less(
 }
 
 
-// Verify that value1 is less than or equal to value2.
+// Verify that value1 is less/greater than or equal to value2.
 // Takes an optional failure description as a third argument.
-#define PQXX_CHECK_LESS_EQUAL(value1, value2, ...)                            \
-  pqxx::test::check_less_equal(                                               \
-    (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
-// Verify that value1 is greater than or equal to value2.
-// Takes an optional failure description as a third argument.
-#define PQXX_CHECK_GREATER_EQUAL(value2, value1, ...)                         \
-  pqxx::test::check_less_equal(                                               \
-    (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+// Work around Visual Studio lacking support for __VA_OPT__.
+#  define PQXX_CHECK_LESS_EQUAL(value1, value2, ...)                          \
+    pqxx::test::check_less_equal(                                             \
+      (value1), #value1, (value2), #value2, ##__VA_ARGS__)
+#  define PQXX_CHECK_GREATER_EQUAL(value2, value1, ...)                       \
+    pqxx::test::check_less_equal(                                             \
+      (value1), #value1, (value2), #value2, ##__VA_ARGS__)
+#else
+#  define PQXX_CHECK_LESS_EQUAL(value1, value2, ...)                          \
+    pqxx::test::check_less_equal(                                             \
+      (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
+#  define PQXX_CHECK_GREATER_EQUAL(value2, value1, ...)                       \
+    pqxx::test::check_less_equal(                                             \
+      (value1), #value1, (value2), #value2 __VA_OPT__(, ) __VA_ARGS__)
+#endif
 template<typename VALUE1, typename VALUE2>
 inline void check_less_equal(
   VALUE1 const &value1, char const text1[], VALUE2 const &value2,
@@ -177,9 +211,15 @@ inline void end_of_statement() {}
 
 // Verify that "action" does not throw an exception.
 // Takes an optional failure description as a second argument.
-#define PQXX_CHECK_SUCCEEDS(action, ...)                                      \
-  pqxx::test::check_succeeds(                                                 \
-    ([&]() { action; }), #action __VA_OPT__(, ) __VA_ARGS__)
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+// Work around Visual Studio lacking support for __VA_OPT__.
+#  define PQXX_CHECK_SUCCEEDS(action, ...)                                    \
+    pqxx::test::check_succeeds(([&]() { action; }), #action, ##__VA_ARGS__)
+#else
+#  define PQXX_CHECK_SUCCEEDS(action, ...)                                    \
+    pqxx::test::check_succeeds(                                               \
+      ([&]() { action; }), #action __VA_OPT__(, ) __VA_ARGS__)
+#endif
 
 template<std::invocable F>
 inline void check_succeeds(
@@ -262,25 +302,52 @@ inline void check_throws_exception(
 }
 
 
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+// Work around Visual Studio lacking support for __VA_OPT__.
+
 // Verify that "action" throws an exception, of any std::exception-based type.
 // Takes an optional failure description as an 2nd argument.
-#define PQXX_CHECK_THROWS_EXCEPTION(action, ...)                              \
-  pqxx::test::check_throws_exception(                                         \
-    ([&]() { return action, 0; }), #action __VA_OPT__(, ) __VA_ARGS__)
+#  define PQXX_CHECK_THROWS_EXCEPTION(action, ...)                            \
+    pqxx::test::check_throws_exception(                                       \
+      ([&]() { return action, 0; }), #action, ##__VA_ARGS__)
 
 // Verify that "action" throws "exception_type" (which is not std::exception).
 // Takes an optional failure description as an 2nd argument.
-#define PQXX_CHECK_THROWS(action, exception_type, ...)                        \
-  pqxx::test::check_throws<exception_type>(                                   \
-    ([&] { return action, 0; }), #action __VA_OPT__(, ) __VA_ARGS__)
+#  define PQXX_CHECK_THROWS(action, exception_type, ...)                      \
+    pqxx::test::check_throws<exception_type>(                                 \
+      ([&] { return action, 0; }), #action, ##__VA_ARGS__)
 
 
 // Verify that "value" is between "lower" (inclusive) and "upper" (exclusive).
 // Takes an optional failure description as a 4th argument.
-#define PQXX_CHECK_BOUNDS(value, lower, upper, ...)                           \
-  pqxx::test::check_bounds(                                                   \
-    (value), #value, (lower), #lower, (upper),                                \
-    #upper __VA_OPT__(, ) __VA_ARGS__)
+#  define PQXX_CHECK_BOUNDS(value, lower, upper, ...)                         \
+    pqxx::test::check_bounds(                                                 \
+      (value), #value, (lower), #lower, (upper), #upper, ##__VA_ARGS__)
+
+#else
+
+// Verify that "action" throws an exception, of any std::exception-based type.
+// Takes an optional failure description as an 2nd argument.
+#  define PQXX_CHECK_THROWS_EXCEPTION(action, ...)                            \
+    pqxx::test::check_throws_exception(                                       \
+      ([&]() { return action, 0; }), #action __VA_OPT__(, ) __VA_ARGS__)
+
+// Verify that "action" throws "exception_type" (which is not std::exception).
+// Takes an optional failure description as an 2nd argument.
+#  define PQXX_CHECK_THROWS(action, exception_type, ...)                      \
+    pqxx::test::check_throws<exception_type>(                                 \
+      ([&] { return action, 0; }), #action __VA_OPT__(, ) __VA_ARGS__)
+
+
+// Verify that "value" is between "lower" (inclusive) and "upper" (exclusive).
+// Takes an optional failure description as a 4th argument.
+#  define PQXX_CHECK_BOUNDS(value, lower, upper, ...)                         \
+    pqxx::test::check_bounds(                                                 \
+      (value), #value, (lower), #lower, (upper),                              \
+      #upper __VA_OPT__(, ) __VA_ARGS__)
+
+
+#endif
 template<typename VALUE, typename LOWER, typename UPPER>
 inline void check_bounds(
   VALUE const &value, char const text[], LOWER const &lower,
