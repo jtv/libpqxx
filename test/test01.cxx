07148d262e9ee7c8595526b722de27732782c830
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-06-19 12:49:44 +0200

    Update test01 a bit.

--- test/test01.cxx
+++ test/test01.cxx
@@ -17,12 +17,27 @@ void test_001()
   // readable name so the library can include it in error messages.
   pqxx::work tx{cx, "test1"};
 
-  // Perform a query on the database, storing result rows in R.
-  pqxx::result const r(tx.exec("SELECT * FROM pg_tables"));
+  // Perform a query on the database, storing result rows in r.
+  pqxx::result const r(tx.exec("SELECT 42, 84"));
 
-  // We're expecting to find some tables...
-  PQXX_CHECK(not std::empty(r), "No tables found.");
+  // We got the one row that we selected.  The result object works a lot like a
+  // normal C++ container.
+  PQXX_CHECK(not std::empty(r), "Result is unexpectedly empty.");
+  PQXX_CHECK_EQUAL(std::size(r), 1, "There's extra data!?");
 
+  // A result is two-dimensional though: it's got rows and columns.
+  PQXX_CHECK_EQUAL(r.columns(), 2, "Unexpected number of columns.");
+
+  // Each row acts pretty much as a container of fields.  (A field is the
+  // intersection of one row and one column.)
+  PQXX_CHECK_EQUAL(r[0][0].view(), "42", "Unexpected data in first field.");
+
+  // To make our transaction take effect, we need to commit it.  If we don't
+  // go through this, the transaction will roll back when the work object gets
+  // destroyed.
+  //
+  // (Of course we made no changes to the database here, so in this case
+  // there's nothing to commit and we might as well leave this out.)
   tx.commit();
 }
 
