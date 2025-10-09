85004c164e73485133f93d1c6f002b48ac4de23b
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-10-09 23:10:35 +0200

    Use a `string_view`, not `string` here.

--- test/test_strconv.cxx
+++ test/test_strconv.cxx
@@ -147,7 +147,7 @@ constexpr char hash_index(std::size_t index)
 template<typename T>
 void check_write(T const &value, std::string_view expected)
 {
-  std::string const name{pqxx::name_type<T>()};
+  std::string_view const name{pqxx::name_type<T>()};
   std::array<char, 1000> buf{};
   for (auto i{0u}; i < std::size(buf); ++i) buf.at(i) = hash_index(i);
 
