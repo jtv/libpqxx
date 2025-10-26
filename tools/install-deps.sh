476bf5503ad3beb23c99eea7a919010e6b40bd3c
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-10-26 12:30:09 +0100

    Better name.

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -18,39 +18,39 @@
 # The script may output shell commands that you'll need to run in your own
 # shell process, to set variables and such.
 #
-# It also logs package installation to /tmp/install.txt.
+# It also logs package installation to /tmp/install.log.
 
 set -Cue -o pipefail
 
 install_archlinux() {
-    pacman --quiet --noconfirm -Sy >>/tmp/install.txt
+    pacman --quiet --noconfirm -Sy >>/tmp/install.log
     pacman --quiet --noconfirm -S \
         autoconf autoconf-archive automake clang cmake cppcheck diffutils \
         libtool make postgresql postgresql-libs python3 shellcheck uv \
-        which yamllint >>/tmp/install.txt
+        which yamllint >>/tmp/install.log
 }
 
 
 install_archlinux_lint() {
-    pacman --quiet --noconfirm -Sy >>/tmp/install.txt
+    pacman --quiet --noconfirm -Sy >>/tmp/install.log
     pacman --quiet --noconfirm -S \
         clang cmake cppcheck diffutils postgresql-libs python3 shellcheck uv \
-        which yamllint >>/tmp/install.txt
+        which yamllint >>/tmp/install.log
 }
 
 
 install_debian() {
     local pgbin
 
-    apt-get -q update >>/tmp/install.txt
+    apt-get -q update >>/tmp/install.log
 
     # Really annoying: there's no package for uv as of yet, so we need to
     # install pipx just so we can use that to install uv.
     DEBIAN_FRONTEND=noninteractive TZ=UTC apt-get -q install -y \
         build-essential autoconf autoconf-archive automake cppcheck clang \
         libpq-dev markdownlint python3 postgresql postgresql-server-dev-all \
-        shellcheck libtool pipx yamllint >>/tmp/install.txt
-    pipx install uv >>/tmp/install.txt
+        shellcheck libtool pipx yamllint >>/tmp/install.log
+    pipx install uv >>/tmp/install.log
 
     pgbin="$(ls -d /usr/lib/postgresql/*/bin)"
 
@@ -59,15 +59,15 @@ install_debian() {
 
 
 install_debian_lint() {
-    apt-get -q update >>/tmp/install.txt
+    apt-get -q update >>/tmp/install.log
 
     # Really annoying: there's no package for uv as of yet, so we need to
     # install pipx just so we can use that to install uv.
     DEBIAN_FRONTEND=noninteractive TZ=UTC apt-get -q install -y \
         build-essential cmake cppcheck clang clang-tidy libpq-dev \
         markdownlint postgresql postgresql-server-dev-all shellcheck yamllint \
-        pipx >>/tmp/install.txt
-    pipx install uv >>/tmp/install.txt
+        pipx >>/tmp/install.log
+    pipx install uv >>/tmp/install.log
 
     echo "export PATH='$PATH:$HOME/.local/bin'"
 }
@@ -75,11 +75,11 @@ install_debian_lint() {
 
 # XXX: Use Arch for this?
 install_debian_codeql() {
-    apt-get -q update >>/tmp/install.txt
+    apt-get -q update >>/tmp/install.log
 
     DEBIAN_FRONTEND=noninteractive TZ=UTC apt-get -q install -y \
-        clang cmake libpq-dev make >>/tmp/install.txt
-    echo '# (No variables to be set.)
+        clang cmake libpq-dev make >>/tmp/install.log
+    echo '# (No variables to be set.)'
 }
 
 
@@ -88,7 +88,7 @@ install_fedora() {
         autoconf autoconf-archive automake cppcheck clang libasan libtool \
         libubsan postgresql postgresql-devel postgresql-server shellcheck uv \
         yamllint \
-        >>/tmp/install.txt
+        >>/tmp/install.log
 
     # I haven't found a curated package for Markdownlint (mdl) on Fedora.
     # That's fine: we run it on the other systems.  Just stub it out.
@@ -99,7 +99,7 @@ install_fedora() {
 install_macos() {
     brew install --quiet \
         autoconf autoconf-archive automake cppcheck libtool postgresql \
-        shellcheck uv yamllint libpq >>/tmp/install.txt
+        shellcheck uv yamllint libpq >>/tmp/install.log
 }
 
 
@@ -125,7 +125,7 @@ install_windows() {
     # output go to stdout because that's where we write our variables, so we
     # let it generate progress information and send the output to stderr.
     choco install cmake llvm mingw ninja postgresql16 --limit-output -y \
-        1> >(tee /tmp/install.txt >&2)
+        1> >(tee /tmp/install.log >&2)
 
     # This is just useless...  To get the installed commands in your path,
     # you run refreshenv.exe AND THEN CLOSE THE SHELL AND OPEN A NEW ONE.
