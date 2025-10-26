3f2c3fbefa4355f84f26856c080726aefcb92b00
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-10-26 12:16:18 +0100

    Upload install logs as artefacts.

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -17,38 +17,40 @@
 #
 # The script may output shell commands that you'll need to run in your own
 # shell process, to set variables and such.
+#
+# It also logs package installation to /tmp/install.txt.
 
 set -Cue -o pipefail
 
 install_archlinux() {
-    pacman --quiet --noconfirm -Sy >/dev/null
+    pacman --quiet --noconfirm -Sy >>/tmp/install.txt
     pacman --quiet --noconfirm -S \
         autoconf autoconf-archive automake clang cmake cppcheck diffutils \
         libtool make postgresql postgresql-libs python3 shellcheck uv \
-        which yamllint >/dev/null
+        which yamllint >>/tmp/install.txt
 }
 
 
 install_archlinux_lint() {
-    pacman --quiet --noconfirm -Sy >/dev/null
+    pacman --quiet --noconfirm -Sy >>/tmp/install.txt
     pacman --quiet --noconfirm -S \
         clang cmake cppcheck diffutils postgresql-libs python3 shellcheck uv \
-        which yamllint >/dev/null
+        which yamllint >>/tmp/install.txt
 }
 
 
 install_debian() {
     local pgbin
 
-    apt-get -q update >/dev/null
+    apt-get -q update >>/tmp/install.txt
 
     # Really annoying: there's no package for uv as of yet, so we need to
     # install pipx just so we can use that to install uv.
     DEBIAN_FRONTEND=noninteractive TZ=UTC apt-get -q install -y \
         build-essential autoconf autoconf-archive automake cppcheck clang \
         libpq-dev markdownlint python3 postgresql postgresql-server-dev-all \
-        shellcheck libtool pipx yamllint >/dev/null
-    pipx install uv >/dev/null
+        shellcheck libtool pipx yamllint >>/tmp/install.txt
+    pipx install uv >>/tmp/install.txt
 
     pgbin="$(ls -d /usr/lib/postgresql/*/bin)"
 
@@ -57,25 +59,27 @@ install_debian() {
 
 
 install_debian_lint() {
-    apt-get -q update >/dev/null
+    apt-get -q update >>/tmp/install.txt
 
     # Really annoying: there's no package for uv as of yet, so we need to
     # install pipx just so we can use that to install uv.
     DEBIAN_FRONTEND=noninteractive TZ=UTC apt-get -q install -y \
         build-essential cmake cppcheck clang clang-tidy libpq-dev \
         markdownlint postgresql postgresql-server-dev-all shellcheck yamllint \
-        pipx >/dev/null
-    pipx install uv >/dev/null
+        pipx >>/tmp/install.txt
+    pipx install uv >>/tmp/install.txt
 
     echo "export PATH='$PATH:$HOME/.local/bin'"
 }
 
 
+# XXX: Use Arch for this?
 install_debian_codeql() {
-    apt-get -q update >/dev/null
+    apt-get -q update >>/tmp/install.txt
 
     DEBIAN_FRONTEND=noninteractive TZ=UTC apt-get -q install -y \
-        clang cmake libpq-dev make
+        clang cmake libpq-dev make >>/tmp/install.txt
+    echo '# (No variables to be set.)
 }
 
 
@@ -84,7 +88,7 @@ install_fedora() {
         autoconf autoconf-archive automake cppcheck clang libasan libtool \
         libubsan postgresql postgresql-devel postgresql-server shellcheck uv \
         yamllint \
-        >/dev/null
+        >>/tmp/install.txt
 
     # I haven't found a curated package for Markdownlint (mdl) on Fedora.
     # That's fine: we run it on the other systems.  Just stub it out.
@@ -95,7 +99,7 @@ install_fedora() {
 install_macos() {
     brew install --quiet \
         autoconf autoconf-archive automake cppcheck libtool postgresql \
-        shellcheck uv yamllint libpq >/dev/null
+        shellcheck uv yamllint libpq >>/tmp/install.txt
 }
 
 
@@ -120,7 +124,8 @@ install_windows() {
     # But if we let this run quietly, then it times out.  And we can't let the
     # output go to stdout because that's where we write our variables, so we
     # let it generate progress information and send the output to stderr.
-    choco install cmake llvm mingw ninja postgresql16 --limit-output -y 1>&2
+    choco install cmake llvm mingw ninja postgresql16 --limit-output -y \
+        1> >(tee /tmp/install.txt >&2)
 
     # This is just useless...  To get the installed commands in your path,
     # you run refreshenv.exe AND THEN CLOSE THE SHELL AND OPEN A NEW ONE.
