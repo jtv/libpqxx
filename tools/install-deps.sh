cc5dcbb9b84e66246383dcf5d3d82ad71b66662d
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-10-27 22:11:54 +0100

    Install `pkg-config-lite` on Windows.

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -103,21 +103,18 @@ install_ubuntu_codeql() {
 }
 
 
-# TODO: Install versionless "postgresql"?  May have to pass password globally.
 # XXX: This may help: https://community.chocolatey.org/packages/postgresql
 install_windows() {
     local pf="/c/Program Files"
     local cmake_bin="$pf/CMake/bin"
     local llvm_bin="$pf/llvm/bin"
     local pg_ver="18"
+    local pg_bin="$pf/PostgreSQL/$pg_ver/bin"
 
-    # For unclear reason, g++ doesn't seem to work.  But to install it you'd
-    # ask for mingw.
+    # The g++ compiler comes with the MinGW package.
     # TODO: Would be good to have this working.
     local pd="/c/ProgramData"
     local mingw_bin="$pd/mingw64/mingw64/bin"
-
-    local pg_bin="$pf/PostgreSQL/$pg_ver/bin"
 
     # This dumps an unacceptable amount of garbage to stderr, even with the
     # --limit-output option which AFAICT does nothing to limit output (and
@@ -126,28 +123,16 @@ install_windows() {
     # But if we let this run quietly, then it times out.  And we can't let the
     # output go to stdout because that's where we write our variables, so we
     # let it generate progress information and send the output to stderr.
-    choco install cmake llvm mingw ninja postgresql$pg_ver --limit-output -y \
-        1>&2 | tee install.log >&2
+    choco install cmake llvm mingw ninja pkg-config-lite postgresql$pg_ver \
+        --limit-output -y 1>&2 | tee install.log >&2
 
     # This is just useless...  To get the installed commands in your path,
     # you run refreshenv.exe AND THEN CLOSE THE SHELL AND OPEN A NEW ONE.
     # Instead, we'll just have to add all these directories to PATH.
-    echo "PATH='$PATH:$cmake_bin:$llvm_bin:$pg_bin'"
+    echo "PATH='$PATH:$cmake_bin:$llvm_bin:$pg_bin:$mingw_bin'"
     echo "export PATH"
 
-    echo >&2 "*** $pf/PostgreSQL/18: $(ls "$pf/PostgreSQL/18") ***" # XXX: DEBUG
-    echo >&2 # XXX: DEBUG
-    echo >&2 "*** $pg_bin: $(ls "$pg_bin") ***" # XXX: DEBUG
-    echo >&2 # XXX: DEBUG
-    echo >&2 "*** $pd: $(ls "$pd") ***" # XXX: DEBUG
-    echo >&2 # XXX: DEBUG
-    echo >&2 "*** $pd/mingw64: $(ls "$pd/mingw64") ***" # XXX: DEBUG
-    echo >&2 # XXX: DEBUG
-    echo >&2 "*** $pd/mingw64/ming64: $(ls "$pd/mingw64/ming64") ***" # XXX: DEBUG
-    echo >&2 # XXX: DEBUG
-    echo >&2 "*** $pd/mingw64/ming64/bin: $(ls "$pd/mingw64/ming64/bin") ***" # XXX: DEBUG
-    echo >&2 # XXX: DEBUG
-    echo >&2 "*** $mingw_bin: $(ls "$mingw_bin") ***" # XXX: DEBUG
+    g++ --version >&2 # XXX: DEBUG
 }
 
 
