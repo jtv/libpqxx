24e24ecd76a841488d2cb1ada67d3878621f7ead
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-11-04 11:23:52 +0100

    Help output for `idock.sh`.

--- tools/idock.sh
+++ tools/idock.sh
@@ -10,6 +10,24 @@
 # 3. Optional command line to run inside the container, e.g. "ksh".
 set -Cue -o pipefail
 
+case "${1:-}" in
+    -h|--help|'')
+        cat <<EOF >&2
+Runs an interactive docker container with less typing.
+
+You can run an optional command line inside the container, or it will run an
+interactive shell by default.
+
+Usage:
+    $0 [docker options] <image> [command line]
+
+Inside the container, the libpqxx source tree will be bind-mounted at
+'/mnt/pqxx'.
+EOF
+        exit 1
+        ;;
+esac
+
 # (This trick is bash-specific.  It's surprisingly difficult in pure POSIX to
 # just get the location of the current script.)
 PQXX="$(dirname $(dirname "$BASH_SOURCE"))"
@@ -17,4 +35,5 @@ MOUNT=/mnt/pqxx
 
 echo "Running docker with '$PQXX' bind-mounted as '$MOUNT'."
 
+# The actual work.
 docker run -ti -v "$PQXX:/mnt/pqxx" $*
