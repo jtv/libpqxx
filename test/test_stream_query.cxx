ef437295057a0ac15ed99d9972637aa7d018fb3b
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-11-17 18:18:37 +0100

    One more "overflow."  Are we done now?

--- test/test_stream_query.cxx
+++ test/test_stream_query.cxx
@@ -154,7 +154,7 @@ void test_stream_parses_awkward_strings()
   PQXX_CHECK(values.at(1).has_value(), "String 'NULL' became a NULL.");
   PQXX_CHECK_EQUAL(
     values.at(1).value_or("empty"), "NULL", "String 'NULL' went badly.");
-  PQXX_CHECK(values[2].has_value(), "String '\\N' became a NULL.");
+  PQXX_CHECK(values.at(2).has_value(), "String '\\N' became a NULL.");
   PQXX_CHECK_EQUAL(
     values.at(2).value_or("empty"), "\\N", "String '\\N' went badly.");
   PQXX_CHECK(values.at(3).has_value(), "String \"'NULL'\" became a NULL.");
