ea088cd98f42b422b9edcd8b3591b529a50d2345
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-11-25 23:00:56 +0100

    Tweak a test.

--- test/test_float.cxx
+++ test/test_float.cxx
@@ -9,17 +9,17 @@ namespace
 /// Test conversions for some floating-point type.
 template<typename T> void infinity_test()
 {
-  T inf{std::numeric_limits<T>::infinity()};
-  std::string inf_string;
-  T back_conversion;
+  T const inf{std::numeric_limits<T>::infinity()};
+  static constexpr auto big_num{static_cast<T>(999999999)};
 
-  inf_string = pqxx::to_string(inf);
+  auto inf_string = pqxx::to_string(inf);
+  T back_conversion;
   pqxx::from_string(inf_string, back_conversion);
-  PQXX_CHECK_LESS(T(999999999), back_conversion);
+  PQXX_CHECK_LESS(big_num, back_conversion);
 
   inf_string = pqxx::to_string(-inf);
   pqxx::from_string(inf_string, back_conversion);
-  PQXX_CHECK_LESS(back_conversion, -T(999999999));
+  PQXX_CHECK_LESS(back_conversion, -big_num);
 }
 
 void test_infinities()
