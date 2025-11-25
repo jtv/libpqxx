2aa523fde60d2befc1988ac8b5769b7adcced81a
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-11-25 18:33:35 +0100

    Forgot to install `which`.

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -118,7 +118,7 @@ install_archlinux_valgrind() {
     (
         pacman --quiet --noconfirm -Sy >>/tmp/install.log
         pacman --quiet --needed --noconfirm -S \
-            cmake ninja postgresql postgresql-libs python3 valgrind \
+            cmake ninja postgresql postgresql-libs python3 valgrind which \
             "$cxxpkg"
     ) >>/tmp/install.log
     echo "export PGHOST=/run/postgresql"
