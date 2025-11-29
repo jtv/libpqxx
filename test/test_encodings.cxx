c4632144208d625332e1f6a6b794582702a4b0f5
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-11-29 21:23:47 +0100

    Build fix.

--- test/test_encodings.cxx
+++ test/test_encodings.cxx
@@ -35,39 +35,39 @@ template<pqxx::encoding_group ENC> std::string const eels;
 template<>
 auto eels<pqxx::encoding_group::big5>{
   "\xa7\xda\xaa\xba\xae\xf0\xb9\xd4\xb2\xee\xb8\xcc\xa5\xfe\xac\x4f\xc5\xc1"
-  "\xb3\xbd"s};
+  "\xb3\xbd"sv};
 /// ASCII-safe: German.
 template<>
 auto const eels<pqxx::encoding_group::ascii_safe>{
-  "Mein Luftkissenfahrzeug ist voll mit Aalen."s};
+  "Mein Luftkissenfahrzeug ist voll mit Aalen."sv};
 /// GB18030: Simplified Chinese.
 template<>
 auto const eels<pqxx::encoding_group::gb18030>{
   "\xce\xd2\xb5\xc4\xc6\xf8\xb5\xe6\xb4\xac\xc0\xef\xd7\xb0\xc2\xfa\xc1\xcb"
-  "\xf7\xa9\xd3\xe3\xa1\xa3"s};
+  "\xf7\xa9\xd3\xe3\xa1\xa3"sv};
 /// GBK: Simplified Chinese.
 template<>
 auto const eels<pqxx::encoding_group::gbk>{
   "\xce\xd2\xb5\xc4\xc6\xf8\xb5\xe6\xb4\xac\xc0\xef\xd7\xb0\xc2\xfa\xc1\xcb"
-  "\xf7\xa9\xd3\xe3\xa1\xa3"s};
+  "\xf7\xa9\xd3\xe3\xa1\xa3"sv};
 /// JOHAB: Korean.
 template<>
 auto const eels<pqxx::encoding_group::johab>{
   "\x90\x81 \xd1\xa1\xa4\xe1\xc7\x61\x9c\x81\xcf\x61\xcb\x61\x93\x65 "
   "\xb8\x77\xb4\xe1\x9d\xa1 \x88\x61\x97\x62 \xc0\x61 "
-  "\xb7\xb6\xb4\xe1\xb6\x61"s};
+  "\xb7\xb6\xb4\xe1\xb6\x61"sv};
 /// SJIS: Japanese.
 template<>
 auto const eels<pqxx::encoding_group::sjis>{
   "\x8e\x84\x82\xcc\x83\x7a\x83\x6f\x81\x5b\x83\x4e\x83\x89\x83\x74\x83\x67"
   "\x82\xcd\x83\x45\x83\x69\x83\x4d\x82\xc5\x82\xa2\x82\xc1\x82\xcf\x82\xa2"
-  "\x82\xc5\x82\xb7"s};
+  "\x82\xc5\x82\xb7"sv};
 /// UHC: Korean.
 template<>
 auto const eels<pqxx::encoding_group::uhc>{
   "\xb3\xbb \xc8\xa3\xb9\xf6\xc5\xa9\xb7\xa1\xc7\xc1\xc6\xae\xb4\xc2 "
   "\xc0\xe5\xbe\xee\xb7\xce \xb0\xa1\xb5\xe6 \xc2\xf7 "
-  "\xc0\xd6\xbe\xee\xbf\xe4"s};
+  "\xc0\xd6\xbe\xee\xbf\xe4"sv};
 
 
 /// A tricky test text.
@@ -76,14 +76,14 @@ auto const eels<pqxx::encoding_group::uhc>{
  */
 template<pqxx::encoding_group ENC> std::string const tricky;
 
-template<> auto const tricky<pqxx::encoding_group::big5>{"\xaa\x58"s};
+template<> auto const tricky<pqxx::encoding_group::big5>{"\xaa\x58"sv};
 // (Yeah such a string is not possible here.)
-template<> auto const tricky<pqxx::encoding_group::ascii_safe>{""s};
-template<> auto const tricky<pqxx::encoding_group::gb18030>{"\x81\x58"s};
-template<> auto const tricky<pqxx::encoding_group::gbk>{"\xaa\x58"s};
-template<> auto const tricky<pqxx::encoding_group::johab>{"\x84\x58"s};
-template<> auto const tricky<pqxx::encoding_group::sjis>{"\x81\x58"s};
-template<> auto const tricky<pqxx::encoding_group::uhc>{"\xa1\x58"s};
+template<> auto const tricky<pqxx::encoding_group::ascii_safe>{""sv};
+template<> auto const tricky<pqxx::encoding_group::gb18030>{"\x81\x58"sv};
+template<> auto const tricky<pqxx::encoding_group::gbk>{"\xaa\x58"sv};
+template<> auto const tricky<pqxx::encoding_group::johab>{"\x84\x58"sv};
+template<> auto const tricky<pqxx::encoding_group::sjis>{"\x81\x58"sv};
+template<> auto const tricky<pqxx::encoding_group::uhc>{"\xa1\x58"sv};
 
 
 /// Test basic sanity of search in encoding group `ENC`.
@@ -137,7 +137,7 @@ template<pqxx::encoding_group ENC> void test_search(std::string_view enc_name)
     "Looks like we fell for an embedded 'X' byte.");
   // Then, a successful search.
   PQXX_CHECK_EQUAL(
-    finder(tricky<ENC> + "Xnn", 0, loc()), std::size(tricky<ENC>),
+    finder(std::string{tricky<ENC>} + "Xnn", 0, loc()), std::size(tricky<ENC>),
     "Did not find 'X' after string with embedded 'X' byte.");
 }
 
