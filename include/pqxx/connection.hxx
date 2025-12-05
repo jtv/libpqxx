61290fdc56383b1f445d09a0b54eb1bb4aadd97d
Jeroen Vermeulen <jtvjtv@gmail.com>
2025-12-05 17:36:04 +0100

    Missed a bit.

--- include/pqxx/connection.hxx
+++ include/pqxx/connection.hxx
@@ -819,7 +819,9 @@ public:
   [[deprecated("Either name your statement, or just parameterise it.")]] void
   prepare(zview definition) &
   {
+#include "pqxx/internal/ignoer-deprecated-pre.hxx"
     return prepare(definition.c_str());
+#include "pqxx/internal/ignoer-deprecated-post.hxx"
   }
 
   /// Drop prepared statement.
