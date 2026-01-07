0ea4935cf86bbf38bfd6c1a04d72ee885e046559
Jeroen Vermeulen <jtvjtv@gmail.com>
2026-01-07 17:44:34 +0100

    Oversight.

--- examples/quick_example.cxx
+++ examples/quick_example.cxx
@@ -30,8 +30,8 @@ int main(int, char *argv[])
   // For querying just one single value, the transaction has a shorthand method
   // query_value().
   //
-  // Use tx.quote() to escape and quote a C++ string for use as an SQL string
-  // in a query's text.
+  // The employee ID shows up in the query as "$1"; that means we'll pass it as
+  // a parameter.  Pass all parameters together in a single "params" object.
   int const employee_id = tx.query_value<int>(
     "SELECT id "
     "FROM Employee "
@@ -43,9 +43,6 @@ int main(int, char *argv[])
   // Update the employee's salary.  Use exec() to perform the command, and
   // no_rows() to check that it produces no result rows.  If the result does
   // contain data, this will throw an exception.
-  //
-  // The employee ID shows up in the query as "$1"; that means we'll pass it as
-  // a parameter.  Pass all parameters together in a single "params" object.
   tx.exec(
       "UPDATE Employee "
       "SET salary = salary + 1 "
