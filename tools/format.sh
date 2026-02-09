0276b91711e1bf80de55a502ae0210ec39fd5395
Jeroen Vermeulen <jtvjtv@gmail.com>
2026-02-09 01:53:58 +0100

    Make `format.sh` format python as well.

--- tools/format.sh
+++ tools/format.sh
@@ -13,3 +13,10 @@ find . -name '*.[ch]xx' -print0 | xargs -0 clang-format -i
 # Reformat CMake files.  (Requires "uv" to be installed.)
 find . -name '*.cmake' -exec \
     uv run --with='pyaml,cmake-format' cmake-format -i '{}' '+'
+
+if which black >/dev/null
+then
+    black -l80
+else
+    uv run --with=black=24.2.0 black -l80
+fi
