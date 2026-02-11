a13b4bd5f3ca9aab6effbb9874202f425c7b366b
Jeroen Vermeulen <jtvjtv@gmail.com>
2026-02-11 20:54:34 +0100

    Shell-interpret ``.

--- tools/compiler_flags.py
+++ tools/compiler_flags.py
@@ -2,7 +2,8 @@
 
 """Check whether the compiler accepts various options.
 
-Prints any fl
+Prints any of the flags that are accepted when used in combination with the
+base command line and the previously accepted flags.
 """
 
 from argparse import (
@@ -10,7 +11,6 @@ from argparse import (
     FileType,
     Namespace,
 )
-from os import getenv
 from pathlib import Path
 from subprocess import (
     DEVNULL,
@@ -19,15 +19,19 @@ from subprocess import (
 from tempfile import NamedTemporaryFile
 
 
+EPILOG = """The flags file may contain comments, on lines starting with '#'
+as the first non-whitespace character.  Any whitespace other than newlines is
+ignored, as are blank lines.
+"""
+
 def parse_args() -> Namespace:
     """Parse command-line options."""
-    parser = ArgumentParser(description=__doc__)
+    parser = ArgumentParser(description=__doc__, epilog=EPILOG)
 
     parser.add_argument(
         "-c",
-        "--cxx",
-        default=getenv("CXX", "c++"),
-        help="C++ compiler.  Defaults to $CXX, or 'c++'.",
+        "--command",
+        help="The basic compiler command line, e.g. 'c++ -std=c++20 -Werror'.",
     )
     parser.add_argument(
         "-f",
@@ -54,18 +58,19 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
-def run_quietly(cmd: list[str | Path]) -> bool:
-    """Run `cmd`, suppressing output; return whether it succeeds."""
-    return run(cmd, stdout=DEVNULL, stderr=DEVNULL).returncode == 0
+def run_quietly(cmd: str) -> bool:
+    """Run `cmd`, suppressing output; return whether it succeeds.
+
+    **This will run `cmd` through shell interpretation.**
+    """
+    return run(cmd, stdout=DEVNULL, stderr=DEVNULL, shell=True).returncode == 0
 
 
 def compiler_accepts(
-    cxx: Path, source: Path, flag: str, prev: None | list[str]
+    command: str, source: Path, flag: str, prev: str = ""
 ) -> bool:
     """Return whether the compiler seems to accept `flag`."""
-    if prev is None:
-        prev = []
-    return run_quietly([cxx, "-c", source] + prev + [flag])
+    return run_quietly(f"{command} -c {source} {prev} {flag}")
 
 
 def make_source():
@@ -89,7 +94,9 @@ def main() -> None:
             flag = flag.strip()
             if flag == "" or flag.startswith("#"):
                 continue
-            if compiler_accepts(args.cxx, Path(src), flag, prev=good_flags):
+            if compiler_accepts(
+                args.command, Path(src), flag, prev=" ".join(good_flags)
+            ):
                 good_flags.append(flag)
 
     sep = " " if args.single_line else "\n"
