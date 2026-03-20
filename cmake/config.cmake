e42017ef7ac2642dc0836d6d397e23e6b84f62fa
Jeroen Vermeulen <jtvjtv@gmail.com>
2026-03-20 20:57:05 +0100

    Even call python interpreter by hand...

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -38,13 +38,17 @@ endforeach()
 # process look as much like the autoconf one as we can.
 configure_file("${CM_CONFIG_H_IN}" "${CONFIG_H}" @ONLY)
 
+find_package(Python3 REQUIRED COMPONENTS Interpreter)
+
 # Then grab the PQXX macros from config.h and write them to config-compiler.h.
 execute_process(
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     RESULT_VARIABLE filter_result
     ERROR_VARIABLE filter_stderr
-    COMMAND ${PROJECT_SOURCE_DIR}/tools/filter_config.py "${CONFIG_H}"
-            "${CONFIG_H_COM}"
+    COMMAND ${Python3_EXECUTABLE}
+        "${PROJECT_SOURCE_DIR}/tools/filter_config.py"
+	"${CONFIG_H}"
+        "${CONFIG_H_COM}"
 )
 # Staggering: execute_process() will fail *silently* on error...
 if(NOT filter_result EQUAL 0)
